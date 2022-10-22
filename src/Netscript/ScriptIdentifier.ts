21a815f1a6c3c8c41d50b52391d93bdede85592f
Jason Goemaat <jason.goemaat@gmail.com>
2022-10-21 23:08:14 -0500

    Revert undesired change to ScriptIdentifier.ts - not sure how it got in my commit

--- src/Netscript/ScriptIdentifier.ts
+++ src/Netscript/ScriptIdentifier.ts
@@ -1,7 +1,6 @@
 import { ScriptArg } from "./ScriptArg";
 
 export type ScriptIdentifier = //This was previously in INetscriptHelper.ts, may move to its own file or a generic types file.
-
     | number
     | {
         scriptname: string;
