838ea46f9371053c96628fbdaeef2d8cecd503c7
Rai <58925163+rai68@users.noreply.github.com>
2022-11-03 19:15:57 +1000

    Update SoftResetButton.tsx

--- src/ui/React/SoftResetButton.tsx
+++ src/ui/React/SoftResetButton.tsx
@@ -28,7 +28,7 @@ export function SoftResetButton({
 
   return (
     <>
-      <Tooltip title="Perform a soft reset. Resets everything as if you had just installed an Augmentation without installing them.">
+      <Tooltip title="Perform a soft reset. Resets everything as if you had just installed Augmentations without installing them.">
         <Button startIcon={<RestartAltIcon />} color={color} onClick={handleButtonClick}>
           Soft Reset
         </Button>
