19dc242499ccb6445c547be0b05599eb2b362e81
Mughur <Michael.cheitel@gmail.com>
2022-12-20 20:05:08 +0200

    expanded corp constants exposure

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -7039,6 +7039,17 @@ declare enum IndustryType {
   RealEstate = "RealEstate",
 }
 
+/** Names of all cities
+ * @public */
+declare enum CityName {
+  Aevum = "Aevum",
+  Chongqing = "Chongqing",
+  Sector12 = "Sector-12",
+  NewTokyo = "New Tokyo",
+  Ishima = "Ishima",
+  Volhaven = "Volhaven",
+}
+
 /** Names of all locations
  * @public */
 declare enum LocationName {
@@ -7426,14 +7437,6 @@ export interface WarehouseAPI {
    * Gets the cost to purchase a warehouse
    * @returns cost
    */
-  getPurchaseWarehouseCost(): number;
-  /**
-   * Gets the cost to upgrade a warehouse to the next level
-   * @param divisionName - Name of the division
-   * @param cityName - Name of the city
-   * @param amt - amount of upgrades defaults to 1
-   * @returns cost to upgrade
-   */
   getUpgradeWarehouseCost(adivisionName: string, acityName: string, amt?: number): number;
   /**
    * Check if you have a warehouse in city
@@ -7483,34 +7486,13 @@ export interface Corporation extends WarehouseAPI, OfficeAPI {
    * @returns cost of the upgrade */
   getUpgradeLevelCost(upgradeName: string): number;
 
-  /** Gets the cost to expand into a new industry
-   * @param industryName - Name of the industry
-   * @returns cost */
-  getExpandIndustryCost(industryName: IndustryType | `${IndustryType}`): number;
-
-  /** Gets the cost to expand into a new city
-   * @returns cost */
-  getExpandCityCost(): number;
-
   /** Get an offer for investment based on you companies current valuation
    * @returns An offer of investment */
   getInvestmentOffer(): InvestmentOffer;
 
-  /** Get list of materials
-   * @returns material names */
-  getMaterialNames(): string[];
-
-  /** Get list of one-time unlockable upgrades
-   * @returns unlockable upgrades names */
-  getUnlockables(): string[];
-
-  /**  Get list of upgrade names
-   * @returns upgrade names */
-  getUpgradeNames(): string[];
-
-  /** Get list of research names
-   * @returns research names */
-  getResearchNames(): string[];
+  /** Get corporation related constants
+   * @returns corporation related constants */
+  getConstants(): CorpConstants;
 
   /** Accept investment based on you companies current valuation
    * @remarks
@@ -7610,6 +7592,88 @@ interface CorporationInfo {
   state: string;
   /** Array of all divisions */
   divisions: Division[];
+}
+
+/**
+ * Corporation related constants
+ * @public
+ */
+interface CorpConstants {
+  /** Name of the corporation */
+  states: string[];
+  /** Funds available */
+  unlocks: string[];
+  /** Revenue per second this cycle */
+  upgrades: string[];
+  /** Expenses per second this cycle */
+  researches: Record<string, string[]>;
+  /** Indicating if the company is public */
+  jobs: string[];
+  /** Total number of shares issues by this corporation */
+  bribeToRepRatio: number;
+  /** Amount of share owned */
+  baseMaxProducts: number;
+  /** Cooldown until shares can be sold again */
+  cityExpandCost: number;
+  /** Amount of acquirable shares. */
+  warehousePurchaseCost: number;
+  /** Price of the shares */
+  coffeeCost: number;
+  /** Fraction of profits issued as dividends */
+  materials: Record<string, materialInfo>;
+  /** Tax applied on your earnings as a shareholder */
+  products: Record<string, productInfo>;
+  /** Your earnings as a shareholder per second this cycle */
+  divisions: Record<string, divisionInfo>;
+}
+
+/**
+ * Corporation related constants
+ * @public
+ */
+interface materialInfo {
+  /** Name of the material */
+  name: string;
+  /** Size of the material */
+  size: number;
+  /** Revenue per second this cycle */
+  prodMult: boolean;
+}
+
+/**
+ * Corporation related constants
+ * @public
+ */
+interface productInfo {
+  /** Product type */
+  type?: string;
+  /** Size of the product */
+  size: number;
+  /** Materials required to make the product */
+  requiredMaterials: string[];
+  /** Division type which makes the product */
+  division: string;
+}
+
+/**
+ * Corporation related constants
+ * @public
+ */
+interface divisionInfo {
+  /** Division type */
+  type: string;
+  /** Cost to expand to the division */
+  cost: number;
+  /** Materials required for production and their amounts */
+  requiredMaterials: Record<string, number>;
+  /** Materials produced */
+  producedMaterials?: string[];
+  /** Whether the division makes materials */
+  makesMaterials: boolean;
+  /** Whether the division makes products */
+  makesProducts: boolean;
+  /** Product type */
+  productType?: string;
 }
 
 /**
