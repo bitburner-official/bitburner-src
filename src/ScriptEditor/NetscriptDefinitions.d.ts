dc371f2f0fd171ab65997e004600d76bdca12e94
zerbosh <115591472+zerbosh@users.noreply.github.com>
2023-01-02 14:51:59 +0100

    added info to getSleeve about storedCycles

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -3568,6 +3568,7 @@ export interface Sleeve {
    * RAM cost: 4 GB
    *
    * Return a person object for this sleeve
+   * storedCycles is the amount of Bonus Time in cycles, each translates to 200ms 
    *
    * @param sleeveNumber - Index of the sleeve to retrieve information.
    * @returns Object containing information about this sleeve.
