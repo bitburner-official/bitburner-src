0c2d402f0b72c09c633e161e520e0f4f66614316
David Walker <d0sboots@gmail.com>
2023-01-07 23:36:55 -0800

    INFRA: Update everything except React (#312)

--- tools/package-electron.sh
+++ tools/package-electron.sh
@@ -10,6 +10,8 @@ mkdir -p .package/dist/ || true
 cp index.html .package
 cp favicon.ico .package
 cp -r electron/* .package
+# steam_appid.txt would end up in the resource dir
+rm .package/steam_appid.txt
 cp -r dist .package
 
 # Install electron sub-dependencies
@@ -20,3 +22,5 @@ cd ..
 BUILD_PLATFORM="${1:-"all"}"
 # And finally build the app.
 npm run electron:packager-$BUILD_PLATFORM
+
+echo .build/* | xargs -n 1 cp electron/steam_appid.txt
