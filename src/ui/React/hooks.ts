8a0ea75416b91ebc12520f882e0609315d2c548a
omuretsu <84951833+Snarling@users.noreply.github.com>
2023-02-11 12:01:52 -0500

    Fix doc for useRerender

--- src/ui/React/hooks.ts
+++ src/ui/React/hooks.ts
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 /** Hook that returns a function for the component. Optionally set an interval to rerender the component.
  * @param autoRerenderTime: Optional. If provided and nonzero, used as the ms interval to automatically call the rerender function.
  */
-export function useRerender(ms?: number) {
+export function useRerender(autoRerenderTime?: number) {
   const setRerender = useState(false)[1];
   const rerender = () => setRerender((old) => !old);
   useEffect(() => {
-    if (!ms) return;
-    const intervalID = setInterval(rerender, ms);
+    if (!autoRerenderTime) return;
+    const intervalID = setInterval(rerender, autoRerenderTime);
     return () => clearInterval(intervalID);
   }, []);
   return rerender;
