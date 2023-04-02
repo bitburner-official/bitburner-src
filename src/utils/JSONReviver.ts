dabbb75f145a4d9c5f48d4ef68bea3b02109c1fc
omuretsu <84951833+Snarling@users.noreply.github.com>
2023-04-02 19:14:04 -0400

    More reviver changes
    Typesafety and docs

--- src/utils/JSONReviver.ts
+++ src/utils/JSONReviver.ts
@@ -1,66 +1,67 @@
 /* Generic Reviver, toJSON, and fromJSON functions used for saving and loading objects */
 
-import { validateObject } from "./Validator";
+import { ObjectValidator, validateObject } from "./Validator";
 
 export interface IReviverValue {
   ctor: string;
   data: any;
 }
 
-// A generic "smart reviver" function.
-// Looks for object values with a `ctor` property and
-// a `data` property. If it finds them, and finds a matching
-// constructor that has a `fromJSON` property on it, it hands
-// off to that `fromJSON` function, passing in the value.
-export function Reviver(key: string, value: IReviverValue | null): any {
-  if (value == null) {
-    return null;
+/**
+ * A generic "smart reviver" function.
+ * Looks for object values with a `ctor` property and a `data` property.
+ * If it finds them, and finds a matching constructor, it hands
+ * off to that `fromJSON` function, passing in the value. */
+export function Reviver(key: string, value: unknown): any {
+  if (typeof value !== "object" || value === null) return value;
+  // @ts-ignore We know it's an object here, just checking type of object properties to type validate.
+  if (typeof value.ctor !== "string" || typeof value.data === "undefined") return value;
+  const typedValue = value as IReviverValue;
+
+  const ctor = Reviver.constructors[typedValue.ctor];
+  if (!ctor) {
+    // Known missing constructors with special handling.
+    switch (typedValue.ctor) {
+      case "AllServersMap":
+        console.warn("Converting AllServersMap for v0.43.1");
+        return typedValue.data;
+    }
+    // Missing constructor with no special handling. Throw error.
+    throw new Error(`Could not locate constructor named ${typedValue.ctor}. If the save data is valid, this is a bug.`);
   }
 
-  if (typeof value === "object" && typeof value.ctor === "string" && typeof value.data !== "undefined") {
-    // Compatibility for version v0.43.1
-    // TODO Remove this eventually
-    if (value.ctor === "AllServersMap") {
-      console.warn("Converting AllServersMap for v0.43.1");
-      return value.data;
-    }
-
-    const ctor = Reviver.constructors[value.ctor];
-
-    if (typeof ctor === "function" && typeof ctor.fromJSON === "function") {
-      const obj = ctor.fromJSON(value);
-      if (ctor.validationData !== undefined) {
-        validateObject(obj, ctor.validationData);
-      }
-      return obj;
-    }
+  const obj = ctor.fromJSON(typedValue);
+  if (ctor.validationData !== undefined) {
+    validateObject(obj, ctor.validationData);
   }
-  return value;
+  return obj;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace Reviver {
-  export const constructors: { [key: string]: any } = {};
+  export const constructors: Partial<
+    Record<
+      string,
+      Function & {
+        fromJSON: (value: IReviverValue) => any;
+        validationData?: ObjectValidator<any>;
+      }
+    >
+  > = {};
 }
 
-// A generic "toJSON" function that creates the data expected
-// by Reviver.
-// `ctorName`  The name of the constructor to use to revive it
-// `obj`       The object being serialized
-// `keys`      (Optional) Array of the properties to serialize,
-//             if not given then all of the objects "own" properties
-//             that don't have function values will be serialized.
-//             (Note: If you list a property in `keys`, it will be serialized
-//             regardless of whether it's an "own" property.)
-// Returns:    The structure (which will then be turned into a string
-//             as part of the JSON.stringify algorithm)
+/**
+ * A generic "toJSON" function that creates the data expected by Reviver.
+ *
+ * @param ctorName String name of the constructor, part of the reviver JSON.
+ * @param obj      The object to convert to stringified data in the reviver JSON.
+ * @param keys     If provided, only these keys will be saved to the reviver JSON data. */
 export function Generic_toJSON<T extends Record<string, any>>(
   ctorName: string,
   obj: T,
   keys?: readonly (keyof T)[],
 ): IReviverValue {
   const data = {} as T;
-  // keys provided: only save data for the provided keys 
+  // keys provided: only save data for the provided keys
   if (keys) {
     for (const key of keys) data[key] = obj[key];
     return { ctor: ctorName, data: data };
@@ -70,14 +71,16 @@ export function Generic_toJSON<T extends Record<string, any>>(
   return { ctor: ctorName, data: data };
 }
 
-// A generic "fromJSON" function for use with Reviver: Just calls the
-// constructor function with no arguments, then applies all of the
-// key/value pairs from the raw data to the instance. Only useful for
-// constructors that can be reasonably called without arguments!
-// Providing an array of keys will only restore data from those keys.
-// `ctor`      The constructor to call
-// `data`      The data to apply
-// Returns:    The object
+/**
+ * A generic "fromJSON" function for use with Reviver: Just calls the
+ * constructor function with no arguments, then applies all of the
+ * key/value pairs from the raw data to the instance. Only useful for
+ * constructors that can be reasonably called without arguments!
+ *
+ * @param ctor The constructor to call
+ * @param data The saved data to restore to the constructed object
+ * @param keys If provided, only these keys will be restored from data.
+ * @returns    The object */
 export function Generic_fromJSON<T extends Record<string, any>>(
   ctor: new () => T,
   // data can actually be anything. We're just pretending it has the right keys for T. Save data is not type validated.
