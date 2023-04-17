a302a50790047d6cb4e418eab3f8c3573d495058
Arron Chapman <unkwntech@gmail.com>
2023-04-17 17:52:26 -0500

    Added hostname to LogBox title.

--- src/ui/React/LogBoxManager.tsx
+++ src/ui/React/LogBoxManager.tsx
@@ -249,7 +249,12 @@ function LogWindow(props: IProps): React.ReactElement {
 
   function title(full = false): string {
     const maxLength = 30;
-    const t = `${script.filename} ${script.args.map((x: ScriptArg): string => `${x}`).join(" ")}`;
+    const server = GetServer(script.server);
+    let hostname = "";
+    if (server !== null) {
+      hostname = `${server.hostname}: `;
+    }
+    const t = `${hostname}${script.filename} ${script.args.map((x: ScriptArg): string => `${x}`).join(" ")}`;
     if (full || t.length <= maxLength) {
       return t;
     }
