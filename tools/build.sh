ae2b8cf87e76f607d4912c204e682e7bfe0e6a53
Snarling <84951833+Snarling@users.noreply.github.com>
2023-04-25 09:12:17 -0400

    Fix build test passing on failed builds

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# builds the game in the root folder
-webpack --mode $1
+# builds the game in the root folder, exit on failure
+webpack --mode $1 || exit 1
 
 # Clear out any files remaining from old builds and recreate folder
 rm -rf .app
