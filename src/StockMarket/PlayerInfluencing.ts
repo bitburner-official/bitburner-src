88cd1aefc1a4eb0f48095e26a2c49dd95df8a019
Nikolai Korolev <korolevns98@gmail.com>
2023-05-24 00:57:34 +0100

    DOC: Fix parameter name in documentation

--- src/StockMarket/PlayerInfluencing.ts
+++ src/StockMarket/PlayerInfluencing.ts
@@ -42,7 +42,7 @@ export function influenceStockThroughServerHack(server: Server, moneyHacked: num
  * server is grown (grow()). The chance of the grow() to increase the stock's
  * second-order forecast is dependent on how much money is added to the server
  * @param {Server} server - Server being grow()n
- * @param {number} moneyHacked - Amount of money added to the server
+ * @param {number} moneyGrown - Amount of money added to the server
  */
 export function influenceStockThroughServerGrow(server: Server, moneyGrown: number): void {
   const orgName = server.organizationName;
