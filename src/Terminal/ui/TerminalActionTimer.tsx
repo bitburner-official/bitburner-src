4f96ad41820aec87136507f82d4c60c07eab242e
bezrodnov <bezrodnov.aleksey@gmail.com>
2023-05-29 20:53:40 +0200

    render progress bar iff action is set

--- src/Terminal/ui/TerminalActionTimer.tsx
+++ src/Terminal/ui/TerminalActionTimer.tsx
@@ -7,5 +7,5 @@ import { Terminal } from "../../Terminal";
 export function TerminalActionTimer(): React.ReactElement {
   useRerender(200);
 
-  return <Typography color="primary">{Terminal.getProgressText()}</Typography>;
+  return <Typography color="primary">{Terminal.action && Terminal.getProgressText()}</Typography>;
 }
