7303cb0a02e9417c543123d870d05e2435040d87
omuretsu <84951833+Snarling@users.noreply.github.com>
2023-06-11 22:22:47 -0400

    Move corp enumlike constants into enums file
    Resolves import loop

--- src/Corporation/Enums.ts
+++ src/Corporation/Enums.ts
@@ -1,3 +1,5 @@
+import { Member } from "src/types";
+
 export enum IndustryType {
   Water = "Water Utilities",
   Spring = "Spring Water",
@@ -51,8 +53,59 @@ export enum CorpUpgradeName {
   ProjectInsight = "Project Insight",
 }
 
-export {
-  materialNames as CorpMaterialName,
-  smartSupplyOptions as SmartSupplyOption,
-  researchNames as CorpResearchName,
-} from "./data/Constants";
+// As const + type for now, convert to enum later
+export const CorpMaterialName = {
+  Water: "Water",
+  Ore: "Ore",
+  Minerals: "Minerals",
+  Food: "Food",
+  Plants: "Plants",
+  Metal: "Metal",
+  Hardware: "Hardware",
+  Chemicals: "Chemicals",
+  Drugs: "Drugs",
+  Robots: "Robots",
+  AiCores: "AI Cores",
+  RealEstate: "Real Estate",
+} as const;
+export type CorpMaterialName = Member<typeof CorpMaterialName>;
+
+// As const + type for now, convert to enum later
+export const SmartSupplyOption = {
+  leftovers: "leftovers",
+  imports: "imports",
+  none: "none",
+} as const;
+export type SmartSupplyOption = Member<typeof SmartSupplyOption>;
+
+// As const + type for now, convert to enum later
+export const CorpBaseResearchName = {
+  Lab: "Hi-Tech R&D Laboratory",
+  AutoBrew: "AutoBrew",
+  AutoParty: "AutoPartyManager",
+  AutoDrug: "Automatic Drug Administration",
+  CPH4Inject: "CPH4 Injections",
+  Drones: "Drones",
+  DronesAssembly: "Drones - Assembly",
+  DronesTransport: "Drones - Transport",
+  GoJuice: "Go-Juice",
+  RecruitHR: "HRBuddy-Recruitment",
+  TrainingHR: "HRBuddy-Training",
+  MarketTa1: "Market-TA.I",
+  MarketTa2: "Market-TA.II",
+  Overclock: "Overclock",
+  SelfCorrectAssemblers: "Self-Correcting Assemblers",
+  Stimu: "Sti.mu",
+} as const;
+export type CorpBaseResearchName = Member<typeof CorpBaseResearchName>;
+
+export const CorpProductResearchName = {
+  Capacity1: "uPgrade: Capacity.I",
+  Capacity2: "uPgrade: Capacity.II",
+  Dashboard: "uPgrade: Dashboard",
+  Fulcrum: "uPgrade: Fulcrum",
+} as const;
+export type CorpProductResearchName = Member<typeof CorpProductResearchName>;
+
+export const CorpResearchName = { ...CorpProductResearchName, ...CorpBaseResearchName };
+export type CorpResearchName = Member<typeof CorpResearchName>;
