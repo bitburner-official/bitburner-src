2740f75146e1214642ded5a8eb55e8a121474f6c
G4mingJon4s <Jthelker@gmail.com>
2023-08-03 20:47:37 +0200

    fourier series implementation

--- src/Worm/Worm.ts
+++ src/Worm/Worm.ts
@@ -6,23 +6,23 @@ import { difficulties } from "./data/difficulties";
 import { WormEvents } from "./WormEvents";
 
 export class Worm {
-	wormLength = 16;
-	minValue = 0;
+	length = 16;
+	minValue = -1;
 	maxValue = 1;
-
+	
+	difficulty: Difficulty = difficulties[0];
+	bonus: BonusType = BonusType.None;
+	
+	frequency = 0.1;
+	amplitude = 1;
+	amplitudes: number[] = [];
 	guess: number[];
-	difficulty: Difficulty;
-	bonus: BonusType;
 
 	storedCycles = 0;
 	processCount = 0;
 
 	constructor() {
-		this.guess = Array.from({ length: this.wormLength }, () => 0);
-		this.bonus = BonusType.None;
-
-		// needed because ts complains
-		this.difficulty = difficulties[0];
+		this.guess = Array.from({ length: this.length }, () => 0);
 
 		this.setDifficulty(difficulties[0]);
 	}
@@ -40,6 +40,10 @@ export class Worm {
 		WormEvents.emit();
 	}
 
+	resetFormula() {
+		this.amplitudes = Array.from({ length: this.difficulty.formulasUsed }, () => Math.random());
+	}
+
 	updateMults() {
 		Player.reapplyMultipliers();
 	}
@@ -47,14 +51,14 @@ export class Worm {
 	setGuess(guess: number[]) {
 		// if new guess is too short, pad 0s at the start. If new guess is too long, remvoe the end
 		this.guess = [
-			...Array.from({ length: Math.max(0, this.wormLength - guess.length) }, () => 0),
-			...guess.slice(0, this.wormLength)
+			...Array.from({ length: Math.max(0, this.length - guess.length) }, () => 0),
+			...guess.slice(0, this.length)
 		];
 	}
 
 	setDifficulty(difficulty: Difficulty) {
 		this.difficulty = difficulty;
-		// in future, might include setting formulas
+		this.resetFormula();
 	}
 
 	toJSON(): IReviverValue {
--- src/Worm/helpers/calculations.ts
+++ src/Worm/helpers/calculations.ts
@@ -2,7 +2,7 @@ import { Multipliers, defaultMultipliers } from "../../PersonObjects/Multipliers
 import { Worm } from "../Worm";
 
 export function isValidGuess(worm: Worm, guess: number[]) {
-	const isRightLength = guess.length === worm.wormLength;
+	const isRightLength = guess.length === worm.length;
 	const allNumbersValid = guess.every(num => isValidNumber(worm, num));
 	return isRightLength && allNumbersValid;
 }
@@ -11,24 +11,36 @@ export function isValidNumber(worm: Worm, number: number) {
 	return worm.minValue <= number && number <= worm.maxValue
 }
 
-export function calculatePerfectWorm(wormLength: number) {
-	return Array.from({ length: wormLength }, () => Math.random());
+export function calculatePerfectWorm(worm: Worm) {
+	// The function used is a simplified form of a fourier series. (1 / numWaves) * globalScalar * [ sum_{1}^{numWaves} amplitude[numWave] * sin(numWave * frequency * (x + 1)) ]
+	// Shifted to the left by 1, f(-1) = 0, for all variables
+	const evalute = (x: number) =>
+		(1 / worm.amplitudes.length)
+		* worm.amplitude
+		* worm.amplitudes.reduce((acc, cur, i) => acc + (cur * Math.sin(i * worm.frequency * (x + 1))), 0);
+
+	return Array.from({ length: worm.length }, (_, i) => evalute(i));
 }
 
+// to alter the evaluation of the fitness, change this function.
+// the current formula is (2-x)/(2+x*k), where k can be any number above -1.
+export const fitnessFunction = (difference: number) => (1 - difference) / (1 + difference * 8);
+
 export function calculateFitness(worm: Worm) {
-	const perfectWorm = calculatePerfectWorm(worm.wormLength);
+	const perfectWorm = calculatePerfectWorm(worm);
 
-	// Might want to square difference ?
-	const difference = perfectWorm.reduce((acc, val, i) => acc + Math.abs(val - worm.guess[i]), 0);
-	const fitness = (worm.wormLength - difference) / worm.wormLength;
+	// console.log(perfectWorm);
 
-	if (fitness < 0) console.error("Difference greater than worm length: FITNESS, DIFFERENCE", fitness, difference);
+	const fitness = perfectWorm.reduce((acc, val, i) => acc + fitnessFunction(Math.abs(val - worm.guess[i])), 0);
 
-	return fitness;
+	console.log(fitness);
+
+	return fitness / worm.length;
 }
 
 export function calculateBonus(fitness: number, difficultyMultiplier: number) {
-	return fitness * difficultyMultiplier;
+	// PLACEHOLDER
+	return fitness * difficultyMultiplier * 15;
 }
 
 export function calculateWormMults(worm: Worm | null): Multipliers {
