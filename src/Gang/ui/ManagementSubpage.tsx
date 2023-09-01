6fc0a2d6e44e255fe6084040435234796a92a922
myCatsName <dlsman@hotmail.com>
2023-08-31 20:49:25 -0600

    Replace description topline with gang name header

--- src/Gang/ui/ManagementSubpage.tsx
+++ src/Gang/ui/ManagementSubpage.tsx
@@ -9,15 +9,16 @@ export function ManagementSubpage(): React.ReactElement {
   const gang = useGang();
   return (
     <>
+      <Typography variant="h4" color="primary">
+        {gang.facName} (your Gang)
+      </Typography>
       <Typography>
-        This page is used to get an overview of your gang's stats and manage gang members.
-        <br />
         <br />
         If a gang member is not earning much money or respect, the task you assigned might be too difficult. Consider
         assigning an easier task, or training them. Tasks closer to the top of the dropdown list are generally easier.
-        Alternatively, low production might be a sign that your wanted level is too high. Consider using the{" "}
+        Alternatively, low production might be a sign that your wanted level is too high. Consider doing{" "}
         {gang.isHackingGang ? "Ethical Hacking or " : ""}
-        "Vigilante Justice" task to lower your wanted level.
+        "Vigilante Justice" to lower your wanted level.
         <br />
         <br />
         Installing Augmentations does NOT reset progress with your Gang. Furthermore, after installing Augmentations,
