d9c15d7a5d19310416751b362ff9b43e08fe320b
Yichi Zhang <yichizhng@gmail.com>
2023-09-19 14:43:33 -0700

    Update PlayerObjectAugmentationMethods.ts

--- src/PersonObjects/Player/PlayerObjectAugmentationMethods.ts
+++ src/PersonObjects/Player/PlayerObjectAugmentationMethods.ts
@@ -1,5 +1,6 @@
 /** Augmentation-related methods for the Player class (PlayerObject) */
 import { calculateEntropy } from "../Grafting/EntropyAccumulation";
+import { staneksGift } from "../../CotMG/Helper";
 
 import type { PlayerObject } from "./PlayerObject";
 
@@ -9,4 +10,5 @@ export function applyEntropy(this: PlayerObject, stacks = 1): void {
   this.reapplyAllSourceFiles();
 
   this.mults = calculateEntropy(stacks);
+  staneksGift.updateMults();
 }
