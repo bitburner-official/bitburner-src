e6774026d3216597c0d063b7cf73d503e588f8e4
myCatsName <dlsman@hotmail.com>
2023-09-19 15:16:14 -0600

    add base regex and typography

--- src/GameOptions/ui/CreditsModal.tsx
+++ src/GameOptions/ui/CreditsModal.tsx
@@ -2,10 +2,29 @@ import React from "react";
 import { Modal } from "../../ui/React/Modal";
 import { Typography, Link, Button } from "@mui/material";
 
+import { CONSTANTS } from "../../Constants";
+import { update } from "lodash";
+
 interface IProps {
   open: boolean;
   onClose: () => void;
 }
+
+const handle = /(\([^)]*\))/gm; //grab everything between () pairs
+const recentPatchData = Array.from(new Set(CONSTANTS.LatestUpdate.match(handle)));
+
+const isDate = (data: string) => {
+  const regex = /^\(last update/gm; //(this) isn't @name, but useful
+  return regex.test(data);
+};
+const upDate = []; //store most recent update date, eg (last updated 9/12/23)
+if (isDate(recentPatchData[0])) upDate.push(recentPatchData[0]);
+
+const handles = recentPatchData
+  .filter((name) => !isDate(name))
+  .sort((a, b) => a.localeCompare(b))
+  .map((name) => name.slice(1, name.length - 1))
+  .join(", ");
 
 export function CreditsModal(props: IProps): React.ReactElement {
   const leadDevs = `danielyxie
@@ -20,7 +39,7 @@ Olivier Gagnon
 or to participate yourself`;
 
   return (
-    <Modal open={props.open} onClose={props.onClose} sx={{ textAlign: "center" }}>
+    <Modal open={props.open} onClose={props.onClose} sx={{ textAlign: "center", maxWidth: "40em" }}>
       <Typography alignContent={"start"} variant="h3">
         Bitburner
       </Typography>
@@ -32,6 +51,9 @@ or to participate yourself`;
       <br />
       <Typography sx={{ textDecoration: "underline" }}>Current Maintainer</Typography>
       <Typography>{currentMaintainer}</Typography>
+      <br />
+      <Typography sx={{ textDecoration: "underline" }}>Recent patch contributors:</Typography>
+      <Typography style={{ whiteSpace: "pre-wrap" }}>{handles}</Typography>
       <br />
       <Typography style={{ whiteSpace: "pre-wrap" }}>
         <Link href={contributorsURL} target="_blank">
