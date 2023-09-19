cc4a8351096073d3850fbb423c9b71b6b436a1a4
Yichi Zhang <yichizhng@gmail.com>
2023-09-19 15:10:23 -0700

    Update StanekDev.tsx

--- src/DevMenu/ui/StanekDev.tsx
+++ src/DevMenu/ui/StanekDev.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { staneksGift } from "../../CotMG/Helper";
+import { Player } from "@Player";
 
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
