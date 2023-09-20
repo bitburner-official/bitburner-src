d94c7498d494974d0b2c3539848617c4dac57721
myCatsName <dlsman@hotmail.com>
2023-09-19 20:57:27 -0600

    contributor handles using const.LatestUpdate

--- src/GameOptions/ui/CreditsModal.tsx
+++ src/GameOptions/ui/CreditsModal.tsx
@@ -3,43 +3,56 @@ import { Modal } from "../../ui/React/Modal";
 import { Typography, Link, Button } from "@mui/material";
 
 import { CONSTANTS } from "../../Constants";
-import { update } from "lodash";
 
 interface IProps {
   open: boolean;
   onClose: () => void;
 }
 
-const handle = /(\([^)]*\))/gm; //grab everything between () pairs
-const recentPatchData = Array.from(new Set(CONSTANTS.LatestUpdate.match(handle)));
+const enclosed = /(\([^)]*\))/gm; //grab everything between () pairs
+const recentPatchData = Array.from(new Set(CONSTANTS.LatestUpdate.match(enclosed)));
 
 const isDate = (data: string) => {
-  const regex = /^\(last update/gm; //(this) isn't @name, but useful
+  const regex = /^\(last update/gm; //(this) isn't @name, but may be useful
   return regex.test(data);
 };
-const upDate = []; //store most recent update date, eg (last updated 9/12/23)
-if (isDate(recentPatchData[0])) upDate.push(recentPatchData[0]);
+const updateMessage = []; //store most recent update message, eg (last updated 9/12/23)
+if (isDate(recentPatchData[0])) updateMessage.push(recentPatchData[0]);
 
-const handles = recentPatchData
-  .filter((name) => !isDate(name))
-  .sort((a, b) => a.localeCompare(b))
-  .map((name) => name.slice(1, name.length - 1))
-  .join(", ");
+const handle: string[] = [];
+for (let i = 0; i < recentPatchData.length; i++) {
+  const atName = /(?:\(?(@[^\s),]*)[),]?)/gm; //make an array of only unique @handles
+  const whatWeWant = recentPatchData[i].replace(atName, "$1");
+  if (isDate(recentPatchData[i]) || !recentPatchData[i].includes(whatWeWant)) continue;
+  if (recentPatchData[i].includes(", ")) {
+    recentPatchData.push(...recentPatchData[i].split(", "));
+    continue;
+  }
+  if (!handle.includes(whatWeWant)) handle.push(whatWeWant);
+}
 
 export function CreditsModal(props: IProps): React.ReactElement {
   const leadDevs = `danielyxie
-Olivier Gagnon
-@Snarling
-`;
+  Olivier Gagnon
+  @Snarling
+  `;
 
   const currentMaintainer = `@Snarling`;
 
+  const handles = handle.sort((a, b) => a.localeCompare(b)).join(", ");
   const contributorsURL = `https://github.com/bitburner-official/bitburner-src/graphs/contributors`;
   const contributorsMessage = `Visit GitHub to see all contributors
 or to participate yourself`;
+  const maxEM = Math.floor(contributorsMessage.length / 2);
 
   return (
-    <Modal open={props.open} onClose={props.onClose} sx={{ textAlign: "center", maxWidth: "40em" }}>
+    <Modal
+      open={props.open}
+      onClose={props.onClose}
+      sx={{
+        textAlign: "center",
+      }}
+    >
       <Typography alignContent={"start"} variant="h3">
         Bitburner
       </Typography>
@@ -53,7 +66,11 @@ or to participate yourself`;
       <Typography>{currentMaintainer}</Typography>
       <br />
       <Typography sx={{ textDecoration: "underline" }}>Recent patch contributors:</Typography>
-      <Typography style={{ whiteSpace: "pre-wrap" }}>{handles}</Typography>
+      <Typography style={{ whiteSpace: "pre-wrap", maxWidth: maxEM + "em", textOverflow: "clip" }}>
+        {/*em unit = character px based, dynamic with font. balance contributor overflow vs longest other message*/}
+        {/*textoverflow "clip" forces very long @names to stretch a single line, it's silly*/}
+        {handles}
+      </Typography>
       <br />
       <Typography style={{ whiteSpace: "pre-wrap" }}>
         <Link href={contributorsURL} target="_blank">
