101dc672fecb19309100581a409e0cad3f1d5c37
Jesse Clark <jjclark1982@gmail.com>
2023-09-20 17:45:01 -0700

    typeset material and product names as text

--- src/Corporation/ui/IndustryProductEquation.tsx
+++ src/Corporation/ui/IndustryProductEquation.tsx
@@ -11,11 +11,11 @@ export function IndustryProductEquation(props: IProps): React.ReactElement {
   const reqs = [];
   for (const [reqMat, reqAmt] of getRecordEntries(props.division.requiredMaterials)) {
     if (!reqAmt) continue;
-    reqs.push(String.raw`${reqAmt}\text{ }${reqMat}`);
+    reqs.push(String.raw`${reqAmt} \text{${reqMat}}`);
   }
-  const prod = props.division.producedMaterials.map((p) => `1\\text{ }${p}`);
+  const prod = props.division.producedMaterials.map((p) => `1 \\text{${p}}`);
   if (props.division.makesProducts) {
-    prod.push("Products");
+    prod.push("\text{Products}");
   }
 
   return <MathJax>{"\\(" + reqs.join("+") + `\\Rightarrow ` + prod.join("+") + "\\)"}</MathJax>;
