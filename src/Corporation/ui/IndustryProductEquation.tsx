7f575eb34cfdd4c4677bfe29d7c2529c68de6978
Jesse Clark <jjclark1982@gmail.com>
2023-10-02 17:13:41 -0700

    UI: Clean up typesetting of Corp ingredient formulas (#819)

--- src/Corporation/ui/IndustryProductEquation.tsx
+++ src/Corporation/ui/IndustryProductEquation.tsx
@@ -11,11 +11,11 @@ export function IndustryProductEquation(props: IProps): React.ReactElement {
   const reqs = [];
   for (const [reqMat, reqAmt] of getRecordEntries(props.division.requiredMaterials)) {
     if (!reqAmt) continue;
-    reqs.push(String.raw`${reqAmt}\text{ }${reqMat}`);
+    reqs.push(String.raw`${reqAmt}\;\textit{${reqMat}}`);
   }
-  const prod = props.division.producedMaterials.map((p) => `1\\text{ }${p}`);
+  const prod = props.division.producedMaterials.map((p) => `1\\;\\textit{${p}}`);
   if (props.division.makesProducts) {
-    prod.push("Products");
+    prod.push("\\textit{Products}");
   }
 
   return <MathJax>{"\\(" + reqs.join("+") + `\\Rightarrow ` + prod.join("+") + "\\)"}</MathJax>;
