bc5789af601e25e76cc1860666ead55f01469bca
Zonoia <altlockg@gmail.com>
2023-10-24 11:11:14 +0200

    MISC: Readthedocs configuration that redirects to this repo (#833)

---
+++ doc/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+
+project = 'Bitburner'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+              ]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['*']
+exclude_patterns = ['doc/index.rst']
+
+# -- Options for HTML output -------------------------------------------------
---
+++ doc/index.rst
@@ -0,0 +1,5 @@
+.. meta::
+   :http-equiv=Refresh: 0; url='https://github.com/bitburner-official/bitburner-src/blob/stable/src/Documentation/doc/index.md'
+
+This link is outdated as documentation for Bitburner has been migrated to an in-game menu, this page should have redirected you to the new location.
+You can also click `here to go to the game's documentation <https://github.com/bitburner-official/bitburner-src/blob/stable/src/Documentation/doc/index.md/>`_.
