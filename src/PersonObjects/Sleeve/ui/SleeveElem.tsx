b90f2abb2f83e92930875dd6ed611feadc8715fb
Sphyxis <kelly.caffet@gmail.com>
2023-10-25 18:10:58 -0600

    Update

--- src/PersonObjects/Sleeve/ui/SleeveElem.tsx
+++ src/PersonObjects/Sleeve/ui/SleeveElem.tsx
@@ -74,7 +74,6 @@ export function SleeveElem(props: SleeveElemProps): React.ReactElement {
   function setTask(): void {
     switch (abc[0]) {
       case "------":
-        props.sleeve.stopWork();
         break;
       case "Work for Company":
         if (getEnumHelper("CompanyName").isMember(abc[1])) props.sleeve.workForCompany(abc[1]);
