3f637ee7e6d5a58fe2421cae32a6b9a0e7b54a0d
Jesse Clark <jjclark1982@gmail.com>
2023-11-12 13:18:09 -0800

    Explain in tooltip why "Apply for Job" button is disabled

--- src/Locations/ui/ApplyToJobButton.tsx
+++ src/Locations/ui/ApplyToJobButton.tsx
@@ -5,8 +5,7 @@ import { CompanyPosition } from "../../Company/CompanyPosition";
 import { getJobRequirementText } from "../../Company/GetJobRequirementText";
 
 import { Player } from "@player";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
+import { ButtonWithTooltip } from "../../ui/Components/ButtonWithTooltip";
 
 interface IProps {
   company: Company;
@@ -17,24 +16,29 @@ interface IProps {
 
 /** React Component for a button that's used to apply for a job */
 export function ApplyToJobButton(props: IProps): React.ReactElement {
-  function getJobRequirementTooltip(): string {
-    const pos = Player.getNextCompanyPosition(props.company, props.entryPosType);
-    if (pos == null) {
-      return "";
-    }
+  const pos = Player.getNextCompanyPosition(props.company, props.entryPosType);
+  let disabledText;
+  let reqs;
 
-    if (!props.company.hasPosition(pos)) {
-      return "";
+  if (pos == null) {
+    disabledText = "You are already at the highest position for your field! No promotion available.";
+  } else {
+    if (!props.company.hasPosition(pos as CompanyPosition)) {
+      disabledText = `You are already at the highest position available at ${props.company.name}. No promotion available.`;
+    } else {
+      reqs = (
+        <>
+          <b>{pos.name}</b>
+          <br />
+          <span dangerouslySetInnerHTML={{ __html: getJobRequirementText(props.company, pos, true) }}></span>
+        </>
+      );
     }
-
-    return getJobRequirementText(props.company, pos, true);
   }
 
   return (
-    <>
-      <Tooltip title={<span dangerouslySetInnerHTML={{ __html: getJobRequirementTooltip() }}></span>}>
-        <Button onClick={props.onClick}>{props.text}</Button>
-      </Tooltip>
-    </>
+    <ButtonWithTooltip normalTooltip={reqs} disabledTooltip={disabledText} onClick={props.onClick}>
+      {props.text}
+    </ButtonWithTooltip>
   );
 }
