7ef01d89196596c646e806da03a84582b3b5e86c
G4mingJon4s <Jthelker@gmail.com>
2023-11-17 16:05:04 +0100

    basic UI solving

--- src/Worm/calculations.ts
+++ src/Worm/calculations.ts
@@ -3,6 +3,8 @@ import { Multipliers, defaultMultipliers } from "../PersonObjects/Multipliers";
 import { getMultiplier } from "./BonusType";
 import { Worm } from "./Worm";
 import { calculateIntelligenceBonus } from "../PersonObjects/formulas/intelligence";
+import { WormInputArray } from "@nsdefs";
+import { isValidInput } from "./Automata";
 
 export const getGuessTime = (threads: number) => 60 * 1000 / (threads * calculateIntelligenceBonus(Player.skills.intelligence, 1));
 
@@ -16,6 +18,19 @@ export function calculateWormMults(worm: Worm | null): Multipliers {
 	} else {
 		return effect;
 	}
+}
+
+export function parseWormInputArray(worm: Worm, v: string): WormInputArray | null {
+	const parsedArray = v.split(",").map(s => s.trim());
+	if (parsedArray.length !== 4) return null;
+	const values: [boolean | null, string | null, number, number] = [
+		parsedArray[0].toLowerCase() === "true" ? true : parsedArray[0].toLowerCase() === "false" ? false : null,
+		isValidInput(worm.data, parsedArray[1]) ? parsedArray[1] : null,
+		Number.parseInt(parsedArray[2]),
+		Number.parseInt(parsedArray[3]),
+	];
+	if (values[0] === null || values[1] === null || Number.isNaN(values[2]) || Number.isNaN(values[3])) return null;
+	return values as WormInputArray;
 }
 
 export function isBipartite(graph: Record<string, Record<string, string>>) {
--- src/Worm/ui/WormInput.tsx
+++ src/Worm/ui/WormInput.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Worm } from '../Worm';
 import { Button, Stack, TextField, Typography } from '@mui/material';
 import { isValidInput } from '../Automata';
+import { parseWormInputArray } from '../calculations';
 
 interface IProps {
 	worm: Worm;
@@ -10,12 +11,21 @@ interface IProps {
 export function WormInput({ worm }: IProps) {
 	const [input, setInput] = useState("");
 	const [state, setState] = useState("");
+	const [guess, setGuess] = useState("");
+	const [result, setResult] = useState("");
 
 	function handleSubmit() {
 		if (!isValidInput(worm.data, input)) return;
 		const result = worm.evaluate(input);
 		if (result === null) setState("INVALID");
 		else setState(result);
+	}
+
+	function handleGuess() {
+		const wormArray = parseWormInputArray(worm, guess);
+		if (wormArray === null) return;
+		const result = worm.solve(wormArray);
+		setResult(result.toString());
 	}
 
 	return (
@@ -29,6 +39,21 @@ export function WormInput({ worm }: IProps) {
 			/>
 			<Button disabled={!isValidInput(worm.data, input)} onClick={handleSubmit}>Submit</Button>
 			{state !== "" && <Typography>Result: {state}</Typography>}
+		</Stack>
+		<Typography>Chosen "Value" Node: {worm.chosenNodes[0]}</Typography>
+		<Typography>Chosen "Indegree" Node: {worm.chosenNodes[1]}</Typography>
+		<Stack direction="row">
+			<TextField
+				value={guess}
+				onChange={event => setGuess(event.target.value)}
+				onSubmit={handleGuess}
+				InputProps={{
+					startAdornment: "[",
+					endAdornment: "]"
+				}}
+			/>
+			<Button disabled={!parseWormInputArray(worm, guess)} onClick={handleGuess}>Guess</Button>
+			{result !== "" && <Typography>Reward: {result}</Typography>}
 		</Stack>
 	</>
 	);
