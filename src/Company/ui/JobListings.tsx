3a7ff7880d59493ee99c3f2211e0ddd6aefd6151
Snarling <84951833+Snarling@users.noreply.github.com>
2023-12-19 05:27:26 -0500

    COMPANY: Show unqualified direct promotion (followup) (#983)

--- src/Company/ui/JobListings.tsx
+++ src/Company/ui/JobListings.tsx
@@ -7,33 +7,32 @@ import { CompanyPositions } from "../CompanyPositions";
 import { ApplyToJobButton } from "./ApplyToJobButton";
 import { Player } from "@player";
 
-interface IJobListingsProps {
+interface JobListingsProps {
   company: Company;
   currentPosition: CompanyPosition | null;
 }
 
-export function JobListings(props: IJobListingsProps): React.ReactElement {
-  const { company, currentPosition } = props;
-
+export function JobListings({ company, currentPosition }: JobListingsProps): React.ReactElement {
+  const jobNames = [...company.companyPositions];
+  const qualifiedJobs = new Set(jobNames.filter((jobName) => Player.isQualified(company, CompanyPositions[jobName])));
   const jobsToShow = [];
-  for (const jobName of company.companyPositions) {
-    const offeredPos = CompanyPositions[jobName];
-    const underqualified = !Player.isQualified(props.company, offeredPos);
-    const isCurrentPosition = jobName == props.currentPosition?.name;
-    const nextPos = offeredPos.nextPosition && CompanyPositions[offeredPos.nextPosition];
-    const overqualified = nextPos != null && Player.isQualified(props.company, nextPos);
-    const shouldShowApplyButton =
-      !isCurrentPosition && !overqualified && (!underqualified || offeredPos.requiredReputation == 0);
-
-    if (shouldShowApplyButton) {
-      jobsToShow.push(offeredPos);
+  for (const jobName of jobNames) {
+    if (jobName === currentPosition?.name) continue;
+    const job = CompanyPositions[jobName];
+    const nextJobName = job.nextPosition;
+    // Don't show a job if we already qualify for a later job offered by this company
+    if (nextJobName && qualifiedJobs.has(nextJobName)) continue;
+    // Don't show a job if we don't qualify for it, unless it's a starting job or a promotion from current job
+    if (!qualifiedJobs.has(jobName) && job.requiredReputation > 0 && jobName !== currentPosition?.nextPosition) {
+      continue;
     }
+    jobsToShow.push(job);
   }
 
   return (
     <>
-      {jobsToShow.map((position) => (
-        <ApplyToJobButton key={position.name} company={company} position={position} currentPosition={currentPosition} />
+      {jobsToShow.map((job) => (
+        <ApplyToJobButton key={job.name} company={company} position={job} qualified={qualifiedJobs.has(job.name)} />
       ))}
     </>
   );
