ef334cb8517dfaff517e38a5fbf8d51f92193f8b
Shy <48560522+shyguy1412@users.noreply.github.com>
2024-02-16 01:33:08 +0100

    BUGFIX: make hacknet node a bit bigger and change unit to em (#1094)
    PR #1070 caused some overflow when having hacknet servers.

--- src/Hacknet/ui/HacknetRoot.tsx
+++ src/Hacknet/ui/HacknetRoot.tsx
@@ -117,7 +117,7 @@ export function HacknetRoot(): React.ReactElement {
 
       {hasHacknetServers() && <Button onClick={() => setOpen(true)}>Spend Hashes on Upgrades</Button>}
 
-      <Box sx={{ display: "grid", width: "100%", gridTemplateColumns: "repeat(auto-fit, 380px)" }}>{nodes}</Box>
+      <Box sx={{ display: "grid", width: "100%", gridTemplateColumns: "repeat(auto-fit, 30em)" }}>{nodes}</Box>
       <HashUpgradeModal open={open} onClose={() => setOpen(false)} />
     </>
   );
