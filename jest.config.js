35f23301dd7ad8b8063211f3ed184f6b4bf4617b
CatLover <152669316+catloversg@users.noreply.github.com>
2024-02-23 00:18:11 +0700

    Run prettier

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   transformIgnorePatterns: ["node_modules/(?!react-markdown)/"],
   testPathIgnorePatterns: [".cypress", "node_modules", "dist"],
   testEnvironment: "./FixJSDOMEnvironment.ts",
-  setupFiles: ['./jest.polyfills.js'],
+  setupFiles: ["./jest.polyfills.js"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/test/__mocks__/fileMock.js",
