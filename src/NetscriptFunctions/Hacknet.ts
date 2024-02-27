3175509a25dc086700ff0c53988278a873ef3e2c
Yichi Zhang <yichizhng@gmail.com>
2024-02-26 19:50:54 -0800

    allow 0 silently

--- src/NetscriptFunctions/Hacknet.ts
+++ src/NetscriptFunctions/Hacknet.ts
@@ -202,8 +202,8 @@ export function NetscriptHacknet(): InternalAPI<IHacknet> {
         const upgName = helpers.string(ctx, "upgName", _upgName);
         const upgTarget = helpers.string(ctx, "upgTarget", _upgTarget);
         const count = Math.floor(helpers.number(ctx, "count", _count));
-        if (!(count > 0)) {
-          throw helpers.errorMessage(ctx, 'count must be positive');
+        if (!(count >= 0)) {
+          throw helpers.errorMessage(ctx, 'count may not be negative');
         }
         if (!hasHacknetServers()) {
           return false;
