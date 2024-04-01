fa74d080afbb82f674f148982cf20b1439a86595
CatLover <152669316+catloversg@users.noreply.github.com>
2024-04-01 11:14:01 +0700

    Run prettier

--- src/BitNode/ui/BitnodeMultipliersDescription.tsx
+++ src/BitNode/ui/BitnodeMultipliersDescription.tsx
@@ -43,7 +43,7 @@ export const BitNodeMultipliersDisplay = ({ n, level }: IProps): React.ReactElem
   const mults = getBitNodeMultipliers(n, level ?? Math.min(Player.sourceFileLvl(n) + 1, maxSfLevel));
 
   return (
-    <Box sx={{ columnCount: 2, columnGap: 1, mb: (n === 1 ? 0 : -2) }}>
+    <Box sx={{ columnCount: 2, columnGap: 1, mb: n === 1 ? 0 : -2 }}>
       <GeneralMults n={n} mults={mults} />
       <SkillMults n={n} mults={mults} />
       <FactionMults n={n} mults={mults} />
