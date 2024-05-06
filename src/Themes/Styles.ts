0706fceffb0d72792744abd63adc1b84f5a4ac35
CatLover <152669316+catloversg@users.noreply.github.com>
2024-05-07 02:09:29 +0700

    Use JetBrainsMono font

--- src/Themes/Styles.ts
+++ src/Themes/Styles.ts
@@ -3,5 +3,5 @@ import { IStyleSettings } from "@nsdefs";
 export const defaultStyles: IStyleSettings = {
   lineHeight: 1.5,
   fontFamily:
-    "Lucida Console, Lucida Sans Unicode, Fira Mono, Consolas, Courier New, Courier, monospace, Times New Roman",
+    "JetBrainsMono, Lucida Console, Lucida Sans Unicode, Fira Mono, Consolas, Courier New, Courier, monospace, Times New Roman",
 };
