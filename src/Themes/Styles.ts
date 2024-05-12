325e64cf3c0d4b5d897937b9302ae0116cfbefb3
CatLover <152669316+catloversg@users.noreply.github.com>
2024-05-12 11:36:37 +0700

    Update based on the feedback

--- src/Themes/Styles.ts
+++ src/Themes/Styles.ts
@@ -3,5 +3,5 @@ import { IStyleSettings } from "@nsdefs";
 export const defaultStyles: IStyleSettings = {
   lineHeight: 1.5,
   fontFamily:
-    "JetBrainsMono, Lucida Console, Lucida Sans Unicode, Fira Mono, Consolas, Courier New, Courier, monospace, Times New Roman",
+    `JetBrainsMono, "Courier New", monospace`,
 };
