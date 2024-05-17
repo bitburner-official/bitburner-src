0e2533d8dda6765a0bc6da0ce73fd04c574f4fd8
CatLover <152669316+catloversg@users.noreply.github.com>
2024-05-17 21:24:38 +0700

    Update based on the feedback

--- src/utils/helpers/getRandomIntInclusive.ts
+++ src/utils/helpers/getRandomIntInclusive.ts
@@ -8,6 +8,12 @@
  * @param max The maximum value in the range.
  */
 export function getRandomIntInclusive(min: number, max: number): number {
+  if (!Number.isInteger(min)) {
+    throw new Error(`Min is not an integer. Min: ${min}.`);
+  }
+  if (!Number.isInteger(max)) {
+    throw new Error(`Max is not an integer. Max: ${max}.`);
+  }
   if (min > max) {
     throw new Error(`Min is greater than max. Min: ${min}. Max: ${max}.`);
   }
