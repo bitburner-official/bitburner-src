99b5bf7250fcc1f8624cd5cdd965272797f65778
muesli4brekkes <muesli4brekkies@gmail.com>
2024-06-03 03:56:48 +0100

    a couple refactors, and nudging stuff around

--- src/Terminal/commands/grep.ts
+++ src/Terminal/commands/grep.ts
@@ -10,10 +10,8 @@ export function grep(args: (string | number | boolean)[]): void {
   if (args.length === 0) {
     return Terminal.error("Incorrect usage of grep command. Usage: grep [search string] ...[optional file path(s)]");
   }
-  const argStrings: string[] = args.map(String);
-  const query: string = argStrings[0];
-  const server: BaseServer | null = GetServer(Player.currentServer);
 
+  const argStrings: string[] = args.map(String);
   const badPaths: string[] = [];
   const argScripts: (Script | TextFile | null)[] = argStrings.slice(1).map((arg) => {
     const script: Script | TextFile | null = hasTextExtension(arg)
@@ -24,32 +22,30 @@ export function grep(args: (string | number | boolean)[]): void {
     }
     return script;
   });
-
   // return early if errors found in file args
   if (badPaths.length) {
     return Terminal.error(`Invalid filename(s): ${badPaths.join(", ")}`);
   }
 
-  const defaultFiles: (Script | TextFile | null)[] = [...(server?.scripts ?? []), ...(server?.textFiles ?? [])].map(
-    (tuple) => tuple[1],
-  );
+  const server: BaseServer | null = GetServer(Player.currentServer);
+  const files: (Script | TextFile | null)[] =
+    argScripts.length !== 0
+      ? argScripts
+      : [...(server?.scripts ?? []), ...(server?.textFiles ?? [])].map((tuple) => tuple[1]);
 
-  const files: (Script | TextFile | null)[] = argScripts.length ? argScripts : defaultFiles;
-
+  const query: string = argStrings[0];
   const result: string = files.reduce((accumulator: string, script) => {
     if (!script) return accumulator;
-    let content: string = "";
-    if ("content" in script) {
-      content = script["content"];
-    } else if ("code" in script) {
-      content = script["code"];
-    }
+
+    const content: string = "content" in script ? script["content"] : script["code"];
     if (!content.includes(query)) return accumulator;
+
     const editedContent: string = content
       .split("\n")
       .filter((line) => line.includes(query))
       .map((line, i) => editLine(line.split(query), query, i))
       .join("\n");
+
     return `${accumulator}\n${script.filename}\n${editedContent}\n`;
   }, "");
 
