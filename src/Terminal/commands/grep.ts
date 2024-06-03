a9151df0faa18ff75d5e16000d1e232112479923
muesli4brekkes <muesli4brekkies@gmail.com>
2024-06-03 13:26:49 +0100

    tweaked formatting, much greppier now. Also added arg to add line numbers

--- src/Terminal/commands/grep.ts
+++ src/Terminal/commands/grep.ts
@@ -6,14 +6,33 @@ import { Script } from "src/Script/Script";
 import { TextFile } from "src/TextFile";
 import { Player } from "@player";
 
+const red: string = "\x1b[31m"; // red
+const def: string = "\x1b[0m"; // default
+const green = "\x1b[32m"; // green
+const cyan = "\x1b[36m"; // cyan
+const magenta = "\x1b[35m"; // Magenta
+
 export function grep(args: (string | number | boolean)[]): void {
   if (!args.length) {
     return Terminal.error("Incorrect usage of grep command. Usage: grep [search string] ...[optional file path(s)]");
   }
 
-  const argStrings: string[] = args.map(String);
+  const query = args[0].toString();
+  const [runArgs, otherArgs]: [string[], string[]] = args.slice(1).reduce(
+    ([runArgs, fileStrings]: [string[], string[]], arg) => {
+      const strArg = arg.toString();
+
+      if (strArg.startsWith("-") || strArg.startsWith("--")) {
+        return [[...runArgs, strArg], fileStrings];
+      } else {
+        return [runArgs, [...fileStrings, strArg]];
+      }
+    },
+    [[], []],
+  );
+
   const badPaths: string[] = [];
-  const argScripts: (Script | TextFile | null)[] = argStrings.slice(1).map((arg) => {
+  const argFiles: (Script | TextFile | null)[] = otherArgs.map((arg) => {
     const script: Script | TextFile | null = hasTextExtension(arg)
       ? Terminal.getTextFile(arg)
       : Terminal.getScript(arg);
@@ -27,12 +46,15 @@ export function grep(args: (string | number | boolean)[]): void {
     return Terminal.error(`Invalid filename(s): ${badPaths.join(", ")}`);
   }
 
+  // passed options
+  const isNumbered: boolean = runArgs.some((arg) => ["-n", "--line-number"].includes(arg));
+  const isMultiscript: boolean = argFiles.length > 1;
+
   const server: BaseServer | null = GetServer(Player.currentServer);
-  const files: (Script | TextFile | null)[] = argScripts.length
-    ? argScripts
+  const files: (Script | TextFile | null)[] = argFiles.length
+    ? argFiles
     : [...(server?.scripts ?? []), ...(server?.textFiles ?? [])].map((tuple) => tuple[1]);
 
-  const query: string = argStrings[0];
   const result: string = files.reduce((accumulator: string, script) => {
     if (!script) return accumulator;
 
@@ -41,18 +63,20 @@ export function grep(args: (string | number | boolean)[]): void {
 
     const editedContent: string = content
       .split("\n")
-      .map((line, i) => (line.includes(query) ? editLine(line.split(query), query, i) : null))
+      .map((line, i) => {
+        if (!line.includes(query)) return null;
+        const prefix: string = `${isMultiscript ? `${magenta}${script.filename}${cyan}:${def}` : ""}${
+          isNumbered ? `${green}${i + 1}${cyan}:${def}` : ""
+        }`;
+        const splits = line.split(query);
+
+        return `${prefix}${splits.join(`${red}${query}${def}`)}`;
+      })
       .filter((line) => line)
       .join("\n");
 
-    return `${accumulator}\n${script.filename}\n${editedContent}\n`;
+    return `${accumulator}${editedContent}\n`;
   }, "");
 
   Terminal.print(result);
-}
-
-function editLine(line: string[], query: string, i: number): string {
-  const red: string = "\x1b[31m"; // red
-  const def: string = "\x1b[0m"; // default
-  return `${i + 1}:${line.join(`${red}${query}${def}`)}`;
 }
