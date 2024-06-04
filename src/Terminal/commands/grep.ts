a7aa0221e93794bf1caa3fc84bd84afa90717c4e
muesli4brekkes <muesli4brekkies@gmail.com>
2024-06-04 20:20:15 +0100

    tweaked so order of arguments no longer matters

--- src/Terminal/commands/grep.ts
+++ src/Terminal/commands/grep.ts
@@ -15,39 +15,40 @@ export function grep(args: (string | number | boolean)[], server: BaseServer): v
     return Terminal.error("Incorrect usage of grep command. Usage: grep [OPTION]... PATTERN [FILE]...");
   }
 
-  const [runArgs, otherArgs]: [string[], string[]] = args.slice(1).reduce(
-    ([runArgs, fileStrings]: [string[], string[]], arg) => {
+  const [optionArgs, otherArgs]: [string[], string[]] = args.reduce(
+    ([runArgs, otherArgs]: [string[], string[]], arg) => {
       const strArg: string = arg.toString();
       if (strArg.startsWith("-") || strArg.startsWith("--")) {
-        return [[...runArgs, strArg], fileStrings];
+        return [[...runArgs, strArg], otherArgs];
       } else {
-        return [runArgs, [...fileStrings, strArg]];
+        return [runArgs, [...otherArgs, strArg]];
       }
     },
     [[], []],
   );
 
-  const badPaths: string[] = [];
-  const argFiles: (Script | TextFile | null)[] = otherArgs.map((arg) => {
+  const badFiles: string[] = [];
+  const validFiles: (Script | TextFile | null)[] = otherArgs.slice(1).map((arg) => {
     const script: Script | TextFile | null = hasTextExtension(arg)
       ? Terminal.getTextFile(arg)
       : Terminal.getScript(arg);
     if (!script) {
-      badPaths.push(arg);
+      badFiles.push(arg);
     }
     return script;
   });
   // return early if errors found in file args
-  if (badPaths.length) {
-    return Terminal.error(`Invalid filename(s): ${badPaths.join(", ")}`);
+  if (badFiles.length) {
+    return Terminal.error(`Invalid filename(s): ${badFiles.join(", ")}`);
   }
 
   // passed options
-  const isNumbered: boolean = runArgs.some((arg) => ["-n", "--line-number"].includes(arg));
-  const isMultiscript: boolean = runArgs.some((arg) => ["-H", "--with-filename"].includes(arg)) || argFiles.length > 1;
-  const isRegExp: boolean = runArgs.some((arg) => ["-G", "--basic-regexp"].includes(arg));
+  const isNumbered: boolean = optionArgs.some((arg) => ["-n", "--line-number"].includes(arg));
+  const isMultiscript: boolean =
+    optionArgs.some((arg) => ["-H", "--with-filename"].includes(arg)) || validFiles.length > 1;
+  const isRegExp: boolean = optionArgs.some((arg) => ["-G", "--basic-regexp"].includes(arg));
 
-  let pattern: string | RegExp = args[0].toString();
+  let pattern: string | RegExp = otherArgs[0];
   if (isRegExp) {
     try {
       pattern = new RegExp(pattern, "g");
@@ -56,11 +57,12 @@ export function grep(args: (string | number | boolean)[], server: BaseServer): v
     }
   }
 
-  const files: (Script | TextFile | null)[] = argFiles.length
-    ? argFiles
+  // search all files on server if no file arguments passed
+  const searchFiles: (Script | TextFile | null)[] = validFiles.length
+    ? validFiles
     : [...(server?.scripts ?? []), ...(server?.textFiles ?? [])].map((tuple) => tuple[1]);
 
-  const result: string = files.reduce((accumulator: string, script) => {
+  const result: string = searchFiles.reduce((accumulator: string, script) => {
     if (!script) return accumulator;
 
     const content: string = "content" in script ? script["content"] : script["code"];
