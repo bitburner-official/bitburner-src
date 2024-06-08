e6f32f111d9ba9094ecb8dda5caa41301b2bdca3
CatLover <152669316+catloversg@users.noreply.github.com>
2024-06-08 18:36:03 +0700

    MISC: Fix wrong help text of cd command

--- src/Terminal/HelpText.ts
+++ src/Terminal/HelpText.ts
@@ -148,8 +148,7 @@ export const HelpTexts: Record<string, string[]> = {
   cd: [
     "Usage: cd [dir]",
     " ",
-    "Change to the specified directory. Note that this works even for directories that don't exist. If you ",
-    "change to a directory that does not exist, it will not be 'created'. Examples:",
+    "Change to the specified directory. If you change to a directory that does not exist, it will not be created. Examples:",
     " ",
     "    cd scripts/hacking",
     " ",
