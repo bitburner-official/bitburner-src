7d7d115836905b399ccadb72cd19104ae4c25fe5
CatLover <152669316+catloversg@users.noreply.github.com>
2024-06-22 21:32:15 +0700

    MISC: Cancel spawned scripts in Bitverse

--- src/NetscriptFunctions.ts
+++ src/NetscriptFunctions.ts
@@ -108,6 +108,8 @@ import { getEnumHelper } from "./utils/EnumHelper";
 import { setDeprecatedProperties, deprecationWarning } from "./utils/DeprecationHelper";
 import { ServerConstants } from "./Server/data/Constants";
 import { assertFunction } from "./Netscript/TypeAssertion";
+import { Router } from "./ui/GameRoot";
+import { Page } from "./ui/Router";
 
 export const enums: NSEnums = {
   CityName,
@@ -736,6 +738,10 @@ export const ns: InternalAPI<NSFull> = {
       const runOpts = helpers.spawnOptions(ctx, _thread_or_opt);
       const args = helpers.scriptArgs(ctx, _args);
       setTimeout(() => {
+        if (Router.page() === Page.BitVerse) {
+          helpers.log(ctx, () => `Script execution is canceled because you are in Bitverse.`);
+          return;
+        }
         const scriptServer = GetServer(ctx.workerScript.hostname);
         if (scriptServer === null) {
           throw helpers.errorMessage(ctx, `Cannot find server ${ctx.workerScript.hostname}`);
