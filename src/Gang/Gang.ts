f09ffdd9f6e89662684c1f42272b97b52a2c9561
CatLover <152669316+catloversg@users.noreply.github.com>
2024-06-27 20:44:47 +0700

    Gang: Fix ns.gang.getRecruitsAvailable

--- src/Gang/Gang.ts
+++ src/Gang/Gang.ts
@@ -317,10 +317,14 @@ export class Gang {
   }
 
   getRecruitsAvailable(): number {
+    if (this.members.length >= GangConstants.MaximumGangMembers) {
+      return 0;
+    }
     const numFreeMembers = 3;
     const recruitCostBase = 5;
-    if (this.members.length < numFreeMembers && this.respect < Math.pow(recruitCostBase, numFreeMembers))
+    if (this.members.length < numFreeMembers && this.respect < Math.pow(recruitCostBase, numFreeMembers)) {
       return numFreeMembers - this.members.length; // if the max possible is less than freeMembers
+    }
     return Math.floor(Math.log(this.respect) / Math.log(recruitCostBase)) + numFreeMembers - this.members.length; //else
   }
 
