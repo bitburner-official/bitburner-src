171cdee4538fc7fb44acf4fdc90642b58fa5ea36
CatLover <152669316+catloversg@users.noreply.github.com>
2024-08-02 18:12:35 +0700

    MISC: Clarify deprecation warning of getTimeSinceLastAug

--- src/NetscriptFunctions.ts
+++ src/NetscriptFunctions.ts
@@ -1633,7 +1633,11 @@ export const ns: InternalAPI<NSFull> = {
     return convertTimeMsToTimeElapsedString(milliseconds, milliPrecision);
   },
   getTimeSinceLastAug: () => () => {
-    deprecationWarning("ns.getTimeSinceLastAug()", "Use ns.getResetInfo().lastAugReset instead.");
+    deprecationWarning(
+      "ns.getTimeSinceLastAug()",
+      "Use ns.getResetInfo().lastAugReset instead. Please note that ns.getResetInfo().lastAugReset does NOT return the " +
+        "same value as ns.getTimeSinceLastAug(). Check the NS API documentation for details.",
+    );
     return Player.playtimeSinceLastAug;
   },
   alert: (ctx) => (_message) => {
