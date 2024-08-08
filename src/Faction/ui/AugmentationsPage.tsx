b680eef16c319b5f29343a62651bff4d09e8da10
CatLover <152669316+catloversg@users.noreply.github.com>
2024-08-08 13:36:14 +0700

    UI: Always show description of faction price multiplier

--- src/Faction/ui/AugmentationsPage.tsx
+++ src/Faction/ui/AugmentationsPage.tsx
@@ -141,48 +141,46 @@ export function AugmentationsPage({ faction }: { faction: Faction }): React.Reac
   );
   const owned = augs.filter((aug) => !purchasable.includes(aug));
 
-  const multiplierComponent =
-    faction.name !== FactionName.ShadowsOfAnarchy ? (
-      <Tooltip
-        title={
-          <Typography>
-            The price of every Augmentation increases for every queued Augmentation and it is reset when you install
-            them.
-          </Typography>
-        }
-      >
-        <Typography>
-          <b>Price multiplier:</b> x {formatBigNumber(getGenericAugmentationPriceMultiplier())}
-        </Typography>
-      </Tooltip>
-    ) : (
-      <Tooltip
-        title={
-          <Typography>
-            This price multiplier increases for each {FactionName.ShadowsOfAnarchy} augmentation already purchased. The
-            multiplier is NOT reset when installing augmentations.
-          </Typography>
-        }
-      >
-        <Typography>
-          <b>Price multiplier:</b> x{" "}
-          {formatBigNumber(
-            Math.pow(
-              CONSTANTS.SoACostMult,
-              augs.filter((augmentationName) => Player.hasAugmentation(augmentationName)).length,
-            ),
-          )}
-          <br />
-          <b>Reputation multiplier:</b> x{" "}
-          {formatBigNumber(
-            Math.pow(
-              CONSTANTS.SoARepMult,
-              augs.filter((augmentationName) => Player.hasAugmentation(augmentationName)).length,
-            ),
-          )}
-        </Typography>
-      </Tooltip>
+  let multiplierDescription;
+  let multiplierComponent;
+  if (faction.name !== FactionName.ShadowsOfAnarchy) {
+    multiplierDescription = (
+      <Typography>
+        The price of every Augmentation increases for every queued Augmentation and it is reset when you install them.
+      </Typography>
     );
+    multiplierComponent = (
+      <Typography>
+        <b>Price multiplier:</b> x {formatBigNumber(getGenericAugmentationPriceMultiplier())}
+      </Typography>
+    );
+  } else {
+    multiplierDescription = (
+      <Typography>
+        This price multiplier increases for each {FactionName.ShadowsOfAnarchy} augmentation already purchased. The
+        multiplier is NOT reset when installing augmentations.
+      </Typography>
+    );
+    multiplierComponent = (
+      <Typography>
+        <b>Price multiplier:</b> x{" "}
+        {formatBigNumber(
+          Math.pow(
+            CONSTANTS.SoACostMult,
+            augs.filter((augmentationName) => Player.hasAugmentation(augmentationName)).length,
+          ),
+        )}
+        <br />
+        <b>Reputation multiplier:</b> x{" "}
+        {formatBigNumber(
+          Math.pow(
+            CONSTANTS.SoARepMult,
+            augs.filter((augmentationName) => Player.hasAugmentation(augmentationName)).length,
+          ),
+        )}
+      </Typography>
+    );
+  }
 
   return (
     <>
@@ -193,8 +191,9 @@ export function AugmentationsPage({ faction }: { faction: Faction }): React.Reac
           <Typography>
             These are all of the Augmentations that are available to purchase from <b>{faction.name}</b>. Augmentations
             are powerful upgrades that will enhance your abilities.
-            <br />
           </Typography>
+          <br />
+          {multiplierDescription}
           <Box
             sx={{
               display: "grid",
