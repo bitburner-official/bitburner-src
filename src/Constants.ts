5785d4fbecac9e6e88212ff2ce9cdd685608b4ba
Snarling <84951833+Snarling@users.noreply.github.com>
2025-03-05 03:13:00 -0500

    2.8 Release Prep
    Update constants.ts
    Update version in package.jsons
    Move changelog to documented changelog

--- src/Constants.ts
+++ src/Constants.ts
@@ -4,9 +4,9 @@
  * Constants for specific mechanics or features will NOT be here.
  */
 export const CONSTANTS = {
-  VersionString: "2.8.0dev",
-  isDevBranch: true,
-  VersionNumber: 41,
+  VersionString: "2.8.0",
+  isDevBranch: false,
+  VersionNumber: 42,
 
   /** Max level for any skill, assuming no multipliers. Determined by max numerical value in javascript for experience
    * and the skill level formula in Player.js. Note that all this means it that when experience hits MAX_INT, then
@@ -109,7 +109,7 @@ export const CONSTANTS = {
 
   // Also update Documentation/doc/changelog.md when appropriate (when doing a release)
   LatestUpdate: `
-## v2.8.0 dev branch: Last updated 01 March 2025
+## v2.8.0 Release: 5 March 2025
 
 ### MAJOR CHANGES
 
