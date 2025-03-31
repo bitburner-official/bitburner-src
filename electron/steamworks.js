8ca93865d6c6d3748aabd1f237ccb383e76fb757
CatLover <152669316+catloversg@users.noreply.github.com>
2025-03-31 21:36:27 +0700

    Fix wrong destructuring

--- electron/steamworks.js
+++ electron/steamworks.js
@@ -52,7 +52,7 @@ let runCallbacksInterval = undefined;
  * @returns {Omit<Client, 'init' | 'runCallbacks'>}
  */
 module.exports.init = (appId) => {
-  const { init: internalInit, runCallbacks, ...api } = nativeBinding;
+  const { init: internalInit, runCallbacks, restartAppIfNecessary, ...api } = nativeBinding;
 
   internalInit(appId);
 
