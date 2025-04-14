68999521f52cf4404e18aff21f7c0c9ffc08d13c
CatLover <152669316+catloversg@users.noreply.github.com>
2025-04-14 10:43:42 +0700

    Update based on feedback

--- electron/achievements.js
+++ electron/achievements.js
@@ -10,27 +10,27 @@ function enableAchievementsInterval(window) {
 
   // This is backward but the game fills in an array called `document.achievements` and we retrieve it from
   // here. Hey if it works it works.
-  const steamAchievements = steamworksClient.achievement.names();
-  log.silly(`All Steam achievements ${JSON.stringify(steamAchievements)}`);
-  const playerAchieved = steamAchievements.filter((achievement) => {
-    return steamworksClient.achievement.isActivated(achievement);
-  });
-  log.debug(`Player has Steam achievements ${JSON.stringify(playerAchieved)}`);
+  const allSteamAchievements = steamworksClient.achievement.names();
+  log.silly(`All Steam achievements ${JSON.stringify(allSteamAchievements)}`);
+  const steamAchievements = allSteamAchievements.filter((achievement) =>
+    steamworksClient.achievement.isActivated(achievement),
+  );
+  log.debug(`Player has Steam achievements ${JSON.stringify(steamAchievements)}`);
   const intervalID = setInterval(async () => {
     try {
       const playerAchievements = await window.webContents.executeJavaScript("document.achievements");
       for (const achievement of playerAchievements) {
         // Don't try activating achievements that don't exist Steam-side
-        if (!steamAchievements.includes(achievement)) {
+        if (!allSteamAchievements.includes(achievement)) {
           continue;
         }
         // Don't spam achievements that have already been recorded
-        if (playerAchieved.includes(achievement)) {
+        if (steamAchievements.includes(achievement)) {
           continue;
         }
         log.info(`Granting Steam achievement ${achievement}`);
         if (steamworksClient.achievement.activate(achievement)) {
-          playerAchieved.push(achievement);
+          steamAchievements.push(achievement);
         } else {
           log.warn(`Cannot grant Steam achievement ${achievement}`);
         }
