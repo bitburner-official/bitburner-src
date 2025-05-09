610dc199d762de76d304a13e13cdec6f7f5c5ffd
CatLover <152669316+catloversg@users.noreply.github.com>
2025-05-10 00:53:37 +0700

    UI: Realign and update text in achievement icons

--- src/Achievements/Achievements.ts
+++ src/Achievements/Achievements.ts
@@ -187,7 +187,7 @@ export const achievements: Record<string, Achievement> = {
   },
   NEUROFLUX_255: {
     ...achievementData.NEUROFLUX_255,
-    Icon: "nf255",
+    Icon: "nfg255",
     Condition: () => Player.augmentations.some((a) => a.name === AugmentationName.NeuroFluxGovernor && a.level >= 255),
   },
   NS2: {
@@ -197,7 +197,7 @@ export const achievements: Record<string, Achievement> = {
   },
   FROZE: {
     ...achievementData.FROZE,
-    Icon: "forze",
+    Icon: "frozen",
     Condition: () => location.href.includes("noScripts"),
   },
   RUNNING_SCRIPTS_1000: {
