c1b82cb4802d5fc2f86c9bdc3a7f1a2300075e6f
whiskeyfur <john.walker.ii@gmail.com>
2025-05-11 14:34:10 -0700

    Update src/utils/IPAddress.ts
    Co-authored-by: David Walker <d0sboots@gmail.com>

--- src/utils/IPAddress.ts
+++ src/utils/IPAddress.ts
@@ -9,9 +9,8 @@ export const createRandomIp = (): IPAddress => {
   // Generates a number like 0.c8f0a07f1d47e8
   const ip = Math.random().toString(16);
   // uses regex to match every 2 characters. [0.][c8][f0][a0][7f][1d][47][e8]
-  const matchArray = ip.match(/../g) as RegExpMatchArray;
   // we only want #1 through #4
-  const sliced = matchArray.slice(1, 5);
+  const sliced = ip.match(/../g).slice(1, 5);
   //convert each to a decimal number
   const asDecimals = sliced.map((x) => parseInt(x, 16));
   // and join them together to make a human readable IP address.
