16b055458ed7f77a79bb2bf6fa037f496b3101d8
Jonas Thelker <jthelker@gmail.com>
2025-05-13 16:14:01 +0200

    fix intentional spaces

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -4845,12 +4845,12 @@ export interface Go {
    *
    * For example, a 5x5 board might look like this:
    *
-   [<br/>
-      "XX.O.",<br/>
-      "X..OO",<br/>
-      ".XO..",<br/>
-      "XXO.#",<br/>
-      ".XO.#",<br/>
+   [<br/>  
+      "XX.O.",<br/>  
+      "X..OO",<br/>  
+      ".XO..",<br/>  
+      "XXO.#",<br/>  
+      ".XO.#",<br/>  
    ]
    *
    * Each string represents a vertical column on the board, and each character in the string represents a point.
@@ -4871,12 +4871,12 @@ export interface Go {
    *
    * For example, a single 5x5 prior move board might look like this:
    *
-   [<br/>
-      "XX.O.",<br/>
-      "X..OO",<br/>
-      ".XO..",<br/>
-      "XXO.#",<br/>
-      ".XO.#",<br/>
+   [<br/>  
+      "XX.O.",<br/>  
+      "X..OO",<br/>  
+      ".XO..",<br/>  
+      "XXO.#",<br/>  
+      ".XO.#",<br/>  
    ]
    */
   getMoveHistory(): string[][];
