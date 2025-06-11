e103f7d02b51da619e2a49bd458b919d7088a4e4
Michael Ficocelli <ficocemt@gmail.com>
2025-06-11 14:16:20 -0400

    IPVGO: Ensure that player promises are changed to "gameOver" once the game is over, rather than preserving a prior mid-game move

--- src/Go/boardAnalysis/goAI.ts
+++ src/Go/boardAnalysis/goAI.ts
@@ -62,6 +62,7 @@ export function handleNextTurn(boardState: BoardState = Go.currentGame, useOffli
     // The game is over. We shouldn't get here in most circumstances,
     // because when the game ends resetAI() will be called to resolve promises.
     // Return an already-resolved promise until a new game is started.
+    resetAI(true);
     return Promise.resolve(gameOver);
   }
   const currentColor = previousColor === GoColor.black ? GoColor.white : GoColor.black;
@@ -137,6 +138,8 @@ export function resetAI(endOfGame = false): void {
     if (playerPromise.resolver) {
       playerPromise.resolver(gameOver);
       playerPromise.resolver = null;
+    } else {
+      playerPromise.nextTurn = Promise.resolve(gameOver);
     }
     if (!endOfGame && !playerPromise.resolver) {
       createPromise(playerPromise);
