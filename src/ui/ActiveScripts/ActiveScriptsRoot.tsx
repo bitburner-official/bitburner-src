cd2d3a532e7497593889e3824ae45357672785d3
Michael Ficocelli <ficocemt@gmail.com>
2025-06-16 07:56:20 -0400

    Update src/ui/ActiveScripts/ActiveScriptsRoot.tsx
    Co-authored-by: catloversg <152669316+catloversg@users.noreply.github.com>

--- src/ui/ActiveScripts/ActiveScriptsRoot.tsx
+++ src/ui/ActiveScripts/ActiveScriptsRoot.tsx
@@ -78,8 +78,6 @@ export function ActiveScriptsRoot({ page }: ComponentProps): React.ReactElement 
               <>
                 If this is set, no error modals will be shown for the next five minutes, and only log errors to the
                 Recent Errors page.
-                <br />
-                The "Suppress error modals" setting in the game options will set this toggle to always be on by default.
               </>
             }
             wrapperStyles={{ marginLeft: "20px" }}
