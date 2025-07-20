a9b91509cf805792dbc968558c87bcf61809790e
David Walker <d0sboots@gmail.com>
2025-07-19 19:10:54 -0700

    prettier

--- src/Documentation/root.ts
+++ src/Documentation/root.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from "../utils/EventEmitter";
 
 export const getPage = (title: string): string => {
   const lang = new Intl.Locale(navigator.language).language;
-  const fallbackLang = "en";  // For untranslated languages
+  const fallbackLang = "en"; // For untranslated languages
   let pageContent = null;
   if (!title.startsWith("nsDoc")) {
     pageContent = AllPages[lang + "/" + title];
