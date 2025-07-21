a2f0caf3807b9cffefd58460bef6459aef5d2896
CatLover <152669316+catloversg@users.noreply.github.com>
2025-07-22 00:35:28 +0700

    Update message

--- src/utils/APIBreaks/3.0.0.ts
+++ src/utils/APIBreaks/3.0.0.ts
@@ -7,9 +7,9 @@ export const breakingChanges300: VersionBreakingChange = {
       brokenAPIs: [],
       info:
         "You cannot use the deprecated VSCode extension to connect to the Steam app via API server now. You have to " +
-        "use Remote API to connect your external editors to Bitburner.\nFor more information, please check the pinned " +
-        "message in #external-editors on Discord (https://discord.gg/TFc3hKD).",
-      showPopUp: true,
+        "use Remote API to connect your external editors to Bitburner.\nFor more information, please check the Remote " +
+        "API page in the Documentation tab. If you have a question, please ask us on #external-editors on Discord (https://discord.gg/TFc3hKD).",
+      showWarning: true,
       doNotSkip: true,
     },
     {
