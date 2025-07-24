7ba1d7ed0019e86e48cc781e7764b66876d1ed47
Michael Ficocelli <ficocemt@gmail.com>
2025-07-24 17:06:40 -0400

    Add achievements for bn15

--- src/Achievements/Achievements.ts
+++ src/Achievements/Achievements.ts
@@ -36,6 +36,7 @@ import { Settings } from "../Settings/Settings";
 import { activateSteamAchievements } from "../Electron";
 import { Go } from "../Go/Go";
 import { type AchievementId, type SFAchievementId, SFAchievementIds } from "./Types";
+import { getDarknetServers } from "../DarkNet/controllers/NetworkMovement";
 
 function assertAchievements(
   achievements: typeof data.achievements,
@@ -582,6 +583,12 @@ export const achievements: Record<AchievementId, Achievement> = {
     Condition: () => false,
     NotInSteam: true,
   },
+  DARKNET_BACKDOOR: {
+    ...achievementData.DARKNET_BACKDOOR,
+    Icon: "locked",
+    Visible: knowAboutBitverse,
+    Condition: () => getDarknetServers().filter((s) => s.backdoorInstalled).length >= 50,
+  },
   CHALLENGE_BN1: {
     ...achievementData.CHALLENGE_BN1,
     Icon: "BN1+",
@@ -670,6 +677,13 @@ export const achievements: Record<AchievementId, Achievement> = {
     Icon: "BN14+",
     Visible: knowAboutBitverse,
     Condition: () => Player.bitNodeN === 14 && isBitNodeFinished() && !Go.moveOrCheatViaApi,
+    NotInSteam: true,
+  },
+  CHALLENGE_BN15: {
+    ...achievementData.CHALLENGE_BN15,
+    Icon: "BN15+",
+    Visible: knowAboutBitverse,
+    Condition: () => Player.augmentations.some((a) => a.name === AugmentationName.TheSword),
     NotInSteam: true,
   },
   BYPASS: {
