9c3bca3543e9959ce898585b1c0430b11f121a30
CatLover <152669316+catloversg@users.noreply.github.com>
2025-07-25 12:55:24 +0700

    Revert VersionNumber

--- src/Constants.ts
+++ src/Constants.ts
@@ -6,7 +6,7 @@
 export const CONSTANTS = {
   VersionString: "3.0.0dev",
   isDevBranch: true,
-  VersionNumber: 44,
+  VersionNumber: 43,
 
   /** Max level for any skill, assuming no multipliers. Determined by max numerical value in javascript for experience
    * and the skill level formula in Player.js. Note that all this means it that when experience hits MAX_INT, then
