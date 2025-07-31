38b276829c7b280e3bb8dfc21e229345484b130a
David Walker <d0sboots@gmail.com>
2025-07-31 00:14:06 -0700

    clearer test layout

--- test/jest/formulas/Skill.test.ts
+++ test/jest/formulas/Skill.test.ts
@@ -18,6 +18,10 @@ describe("calculateSkill", () => {
     expect(calculateSkill(calculateExp(skill, 3.3), 3.3)).toBe(skill);
   });
   test("Special cases", () => {
+    expect(() => calculateExp(NaN)).toThrow();
+    expect(calculateExp(Infinity)).toBe(Number.MAX_VALUE);
+    expect(calculateExp(-Infinity)).toBe(0);
+
     // In all these cases, the XP is clamped to 0. With a big enough mult,
     // this gets converted back to a larger skill.
     expect(calculateSkill(calculateExp(0))).toBe(1);
@@ -26,8 +30,5 @@ describe("calculateSkill", () => {
     expect(calculateSkill(calculateExp(1))).toBe(1);
     expect(calculateSkill(calculateExp(1, 1.4), 1.4)).toBe(1);
     expect(calculateSkill(calculateExp(1, 3.3), 3.3)).toBe(3);
-    expect(() => calculateExp(NaN)).toThrow();
-    expect(calculateExp(Infinity)).toBe(Number.MAX_VALUE);
-    expect(calculateExp(-Infinity)).toBe(0);
   });
 });
