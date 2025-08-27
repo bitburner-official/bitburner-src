df6e614c0d8eca8e19de5caa054c1a5698f6840c
CatLover <152669316+catloversg@users.noreply.github.com>
2025-08-27 21:43:22 +0700

    MISC: Update version number

--- src/Constants.ts
+++ src/Constants.ts
@@ -6,7 +6,7 @@
 export const CONSTANTS = {
   VersionString: "3.0.0dev",
   isDevBranch: true,
-  VersionNumber: 43,
+  VersionNumber: 44,
 
   /** Max level for any skill, assuming no multipliers. Determined by max numerical value in javascript for experience
    * and the skill level formula in Player.js. Note that all this means it that when experience hits MAX_INT, then
