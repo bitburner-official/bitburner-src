1883af6c387afa38cc2aa8d5af617ac932982c27
catloversg <152669316+catloversg@users.noreply.github.com>
2025-10-07 13:48:18 +0700

    TOOLS: Enable linting in test folder (#2337)

--- test/jest/config/babelTransform.js
+++ test/jest/config/babelTransform.js
@@ -1,16 +1,18 @@
-const babelJest = require("babel-jest").default;
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import babelJest from "babel-jest";
 
-module.exports = {
-  process(sourceText, sourcePath, options) {
-    const alias = options?.config?.moduleNameMapper;
-    const extensions = options.config.moduleFileExtensions;
-    const babelTransformer = babelJest.createTransformer({
-      presets: [["@babel/preset-react"], ["@babel/preset-env"], ["@babel/preset-typescript"]],
-      plugins: [["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions }]],
-      babelrc: false,
-      configFile: false,
-    });
+export function process(sourceText, sourcePath, options) {
+  const alias = options?.config?.moduleNameMapper;
+  const extensions = options.config.moduleFileExtensions;
+  const babelTransformer = babelJest.createTransformer({
+    presets: [["@babel/preset-react"], ["@babel/preset-env"], ["@babel/preset-typescript"]],
+    plugins: [["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions }]],
+    babelrc: false,
+    configFile: false,
+  });
 
-    return babelTransformer.process(sourceText, sourcePath, options);
-  },
-};
+  return babelTransformer.process(sourceText, sourcePath, options);
+}
