c0b4c55bcd509043fc5bb42293395ce420afe907
CatLover <152669316+catloversg@users.noreply.github.com>
2025-10-15 17:19:15 +0700

    Refactor last commit

--- src/Server/AllServers.ts
+++ src/Server/AllServers.ts
@@ -124,15 +124,19 @@ export function createUniqueRandomIp(): IPAddress {
 
 // Safely add a Server to the AllServers map
 export function AddToAllServers(server: Server | HacknetServer | DarknetServer): void {
-  if (GetServer(server.hostname)) {
-    console.error(`The hostname of the server that's being added is: ${server.hostname}`);
-    console.error(`The server that already has this hostname is: ${AllServers[server.hostname].hostname}`);
-    throw new Error(`Error: Trying to add a server with an existing hostname. Hostname: ${server.hostname}.`);
+  let existingServer = GetServer(server.hostname);
+  if (existingServer) {
+    throw new Error(
+      `Trying to add a server with an existing hostname. New server: ${server.hostname} (${server.ip}). ` +
+        `Existing server: ${existingServer.hostname} (IP: ${existingServer.ip}).`,
+    );
   }
-  if (GetServer(server.ip)) {
-    console.error(`The IP of the server that's being added is: ${server.ip}`);
-    console.error(`The server that already has this IP is: ${AllServers[server.hostname].ip}`);
-    throw new Error(`Error: Trying to add a server with an existing IP. IP: ${server.ip}.`);
+  existingServer = GetServer(server.ip);
+  if (existingServer) {
+    throw new Error(
+      `Trying to add a server with an existing IP. New server: ${server.hostname} (${server.ip}). ` +
+        `Existing server: ${existingServer.hostname} (IP: ${existingServer.ip}).`,
+    );
   }
 
   AllServers[server.hostname] = server;
