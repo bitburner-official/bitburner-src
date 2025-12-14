bf2c69615511a9805fed1c8654ae66de6a7026f1
Paul Setzer <1337JiveTurkey@gmail.com>
2025-12-14 13:29:43 -0500

    DOCUMENTATION: Change a computer to a server
    Co-authored-by: catloversg <152669316+catloversg@users.noreply.github.com>

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -4057,7 +4057,7 @@ export interface CodingContract {
    * @remarks
    * RAM cost: 2 GB
    *
-   * Generate a dummy contract on the current computer with no reward. Used to test various algorithms.
+   * Generate a dummy contract on the current server with no reward. Used to test various algorithms.
    *
    * This function will return null and not generate a contract if the randomized contract name is the same as another contract's name.
    *
