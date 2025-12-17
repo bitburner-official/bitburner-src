21c5de3e06154896eab0151db714eface9fe0cd7
CatLover <152669316+catloversg@users.noreply.github.com>
2025-12-17 17:29:36 +0700

    CODEBASE: Refactor code related to in-game documentation link

--- src/ui/React/NsApiDocumentationLink.tsx
+++ src/ui/React/NsApiDocumentationLink.tsx
@@ -1,29 +1,18 @@
 import React from "react";
-import { Link } from "@mui/material";
-import { Settings } from "../../Settings/Settings";
-import { Router } from "../GameRoot";
-import { Page } from "../Router";
-import { defaultNsApiPage, openDocExternally } from "./Documentation";
+import { defaultNsApiPage } from "./Documentation";
+import { DocumentationLink } from "./DocumentationLink";
 
 export function NsApiDocumentationLink(): React.ReactElement {
   return (
-    <Link
-      target="_blank"
-      onClick={(event) => {
-        if (event.ctrlKey) {
-          openDocExternally(defaultNsApiPage);
-          return;
-        }
-        Router.toPage(Page.Documentation, { docPage: defaultNsApiPage });
-      }}
+    <DocumentationLink
+      page={defaultNsApiPage}
       fontSize="1.2rem"
-      color={Settings.theme.info}
       sx={{
         textDecorationThickness: "3px",
         textUnderlineOffset: "5px",
       }}
     >
       NS API documentation
-    </Link>
+    </DocumentationLink>
   );
 }
