cc1053e6cbfb8a5e40cf6d0a1821a374910e0d93
CatLover <152669316+catloversg@users.noreply.github.com>
2025-12-20 00:27:35 +0700

    Rewrite loadDarkNet

--- src/DarkNet/effects/SaveLoad.ts
+++ src/DarkNet/effects/SaveLoad.ts
@@ -1,5 +1,6 @@
 import { DarknetState } from "../models/DarknetState";
 import { migrateLegacyImmobileServers } from "../controllers/NetworkGenerator";
+import { assertObject } from "../../utils/TypeAssertion";
 
 export type DarknetSaveFormat = {
   storedCycles: number;
@@ -11,30 +12,21 @@ export function getDarkNetSave(): DarknetSaveFormat {
   };
 }
 
-export function loadDarkNet(data: unknown) {
-  /** Function for ending the loading process, showing an error if there is one, and indicating load success/failure */
-  function showError(error: unknown): boolean {
-    console.warn("Encountered the following issue while loading Darknet savedata:");
-    console.error(error);
-    console.warn("Savedata:");
-    console.error(data);
-    return true; // the default state will work fine if something goes wrong, or if no existing save is present
-  }
-  if (!data) return showError("There was no Darknet savedata");
-  // Parsing the savedata
-  if (typeof data !== "string") return showError("Savedata was not a string");
-  let parsedData;
-  try {
-    parsedData = JSON.parse(data) as unknown;
-  } catch (e) {
-    return showError(`Cannot JSON.parse the savedata: ${data}`);
-  }
-  if (!parsedData || typeof parsedData !== "object") return showError("Parsed savedata was not an object");
-  if (!Object.keys(parsedData).includes("storedCycles")) return showError("Parsed savedata was missing storedCycles");
-  const storedCycles: number = +(parsedData as DarknetSaveFormat).storedCycles;
-  if (isNaN(storedCycles)) return showError("Parsed savedata storedCycles was not a number");
-
-  DarknetState.storedCycles = storedCycles < 0 ? 0 : storedCycles;
-
+export function loadDarkNet(saveString: unknown): void {
   migrateLegacyImmobileServers();
+  if (saveString == null || typeof saveString !== "string" || saveString === "") {
+    return;
+  }
+  try {
+    const parsedData: unknown = JSON.parse(saveString);
+    assertObject(parsedData);
+    const { storedCycles } = parsedData;
+    if (typeof storedCycles !== "number" || !Number.isFinite(storedCycles)) {
+      throw new Error(`Invalid storedCycles: ${storedCycles}`);
+    }
+    DarknetState.storedCycles = storedCycles < 0 ? 0 : storedCycles;
+  } catch (error) {
+    console.error(error);
+    console.error("Invalid DarkNet data:", saveString);
+  }
 }
