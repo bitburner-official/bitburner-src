ede6c07365da4e0f8a500ec867e48a78d3021c21
MrBrown24 <matthew.brown.1302@gmail.com>
2026-01-25 04:26:23 -0600

    Removed Currency Symbol Altogether From 150b Msg

--- src/Corporation/Corporation.ts
+++ src/Corporation/Corporation.ts
@@ -22,8 +22,6 @@ import { createEnumKeyedRecord, getRecordValues } from "../Types/Record";
 import { getKeyList } from "../utils/helpers/getKeyList";
 import { assertObject } from "../utils/TypeAssertion";
 
-import { Settings } from "../Settings/Settings";
-
 export const CorporationPromise: PromisePair<CorpStateName> = { promise: null, resolve: null };
 
 interface ICorporationParams {
@@ -152,7 +150,7 @@ export class Corporation {
           dialogBoxCreate(
             "There was an error calculating your Corporations funds and they got reset to 0. " +
               "This is a bug. Please report to game developer.\n\n" +
-              `(Your funds have been set to ${Settings.CurrencySymbol}150b for the inconvenience)`,
+              "(Your funds have been set to 150b for the inconvenience)",
           );
           this.funds = 150e9;
         }
