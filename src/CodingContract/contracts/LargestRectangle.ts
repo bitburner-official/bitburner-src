b6d5bc8ad7a55638f3a5534518e9e234b58b1294
Misha279-UA <rybakov.misha22@gmail.com>
2026-02-20 16:15:02 +0200

    finished the matrix cct
    both ccts are finished!

--- src/CodingContract/contracts/LargestRectangle.ts
+++ src/CodingContract/contracts/LargestRectangle.ts
@@ -1,46 +1,45 @@
 import { getRandomIntInclusive } from "../../utils/helpers/getRandomIntInclusive";
 import { CodingContractTypes } from "../ContractTypes";
 import { CodingContractName } from "@enums";
-
-export const largestRectangle: Pick<CodingContractTypes, CodingContractName.LargestRectangleIHistogram> = {
+//279
+export const largestRectangle: Pick<
+  CodingContractTypes,
+  CodingContractName.LargestRectangleIHistogram | CodingContractName.LargestRectangleIIMatrix
+> = {
   [CodingContractName.LargestRectangleIHistogram]: {
     desc: (data: number[]): string => {
-      let gridString = "";
-      for (const line of data) {
-        gridString += `${line.toString()},`;
-      }
       return [
-        "HISTOGRAM TAMEPLATE 279 279 279:\n\n",
-        `  [${gridString}]\n\n`,
+        "You are given a histogram represented by an array of non-negative integers, where each value denotes the height of a bar and each bar has width 1:\n\n",
+        `  [${data}]\n\n`,
+        "Your task is to find the contiguous sublist of indices [L, R] such that the rectangle formed by bars from index L to R (inclusive) has the maximum possible area.\n\n",
 
-        "Examples:\n\n",
-        "    [[0,1,0,0,0],\n",
-        "     [0,0,0,1,0]]\n",
-        "\n",
-        "Answer: '[[0,0],[0,1]]'\n\n",
-        "    [[0,1],\n",
-        "     [1,0]]\n",
-        "\n",
-        "Answer: '[[0,0],[0,0]]'",
-      ].join(" ");
+        "Examples:\n",
+        "    [3,2,7,9,8,4]\n",
+        "Answer: '[2, 4]'\n",
+        "    [0,1,2,3,4,5,6,7,8,9]\n",
+        "Answer: '[5,9]'",
+      ].join("");
     },
     difficulty: 3, //for now
     generate: (): number[] => {
       const numRows: number = getRandomIntInclusive(5, 30);
 
-      const grid: number[] = [];
-      grid.length = numRows;
+      const histogram: number[] = [];
+      histogram.length = numRows;
       for (let i = 0; i < numRows; ++i) {
-        grid[i] = getRandomIntInclusive(0, 9);
+        histogram[i] = getRandomIntInclusive(0, 9);
       }
 
-      return grid;
+      return histogram;
     },
     getAnswer: (data) => {
       return null;
     },
     solver: (data, answer) => {
-      if (answer[0] < 0 || answer[1] > data.length) return false;
+      if (
+        answer[0] < 0 || answer[0] > data.length ||
+        answer[1] < 0 || answer[1] > data.length 
+      ) return false;
       let maxArea = 0;
       for (let i = 0; i < data.length; i++) {
         if (data[i] > 0) {
@@ -53,7 +52,7 @@ export const largestRectangle: Pick<CodingContractTypes, CodingContractName.Larg
             right++;
           }
           if ((right - left + 1) * data[i] > maxArea) {
-            maxArea = (right - left + 1) * data[i];
+            maxArea = (Math.abs(right - left) + 1) * data[i];
           }
         }
       }
@@ -84,128 +83,137 @@ export const largestRectangle: Pick<CodingContractTypes, CodingContractName.Larg
       return ans != null;
     },
   },
+  [CodingContractName.LargestRectangleIIMatrix]: {
+    desc: (data: number[][]): string => {
+      let gridString = "";
+      for (const line of data) {
+        gridString += `${line.toString()},\n`;
+      }
+      return [
+        "You are given a binary matrix consisting only of 0s and 1s:\n\n",
+        `${gridString}\n\n`,
+        "Your task is to find the two corners of the largest rectangle ([[x1,y1],[x2,y2]]), that does not contain any 1s.\n\n",
+
+        "Examples:\n",
+        "    [[1,0,0],\n",
+        "     [0,0,0]]\n",
+        "Answer: '[[1,0],[2,1]]'\n",
+        "    [[0,0,0,1],\n",
+        "     [0,0,0,0],\n",
+        "     [0,0,1,0],\n",
+        "     [0,0,0,1]]\n",
+        "Answer: '[[0,0],[1,3]]'\n",
+      ].join("");
+    },
+    difficulty: 3, //for now
+    generate: (): (0|1)[][] => {
+      const numRows: number = getRandomIntInclusive(4, 15);
+      const numColumns: number = getRandomIntInclusive(4, 15);
+
+      const grid: (1 | 0)[][] = [];
+      grid.length = numRows;
+      for (let i = 0; i < numRows; ++i) {
+        grid[i] = [];
+        grid[i].length = numColumns;
+        grid[i].fill(0);
+      }
+
+      for (let r = 0; r < numRows; ++r) {
+        for (let c = 0; c < numColumns; ++c) {
+          if (r === 0 && c === 0) {
+            continue;
+          }
+          if (r === numRows - 1 && c === numColumns - 1) {
+            continue;
+          }
+
+          // 15% chance of an element being an obstacle
+          if (Math.random() < 0.15) {
+            grid[r][c] = 1;
+          }
+        }
+      }
+
+      return grid;
+    },
+    getAnswer: (data) => {
+      return null;
+    },
+    solver: (data, answer) => {
+      if (
+        answer[0][0] < 0 || answer[0][0] > data[0].length - 1 ||
+        answer[0][1] < 0 || answer[0][1] > data.length - 1 ||
+        answer[1][0] < 0 || answer[1][0] > data[0].length - 1 ||
+        answer[1][1] < 0 || answer[1][1] > data.length - 1
+      ) return false;
+
+      let scanned = '';
+      for (
+        let i = Math.min(answer[0][1], answer[1][1]);
+        i <= Math.max(answer[0][1], answer[1][1]);
+        i++
+      ) {
+        scanned += data[i].slice(
+          Math.min(answer[0][0], answer[1][0]),
+          Math.max(answer[0][0], answer[1][0]) + 1
+        ).toString();
+      }
+      if (scanned.includes("1")) return false;
+
+      let histograms = Array.from({ length: data.length }, () => Array(data[0].length).fill(0));
+      for (let i = 0; i < data[0].length; i++) {
+        let count = 0;
+        for (let j = 0; j < data.length; j++) {
+          if (data[j][i] == 0) {
+            count++;
+          } else {
+            count = 0;
+          }
+          histograms[j][i] = count;
+        }
+      }
+      let maxArea = 0;
+      for (let i = 0; i < histograms.length; i++) {
+        for (let j = 0; j < histograms[0].length; j++) {
+          if (histograms[i][j] > 0) {
+            let left = j;
+            let right = j;
+            while (histograms[i]?.[left - 1] >= histograms[i][j]) {
+              left--;
+            }
+            while (histograms[i]?.[right + 1] >= histograms[i][j]) {
+              right++;
+            }
+            if ((right - left + 1) * histograms[i][j] > maxArea) {
+              maxArea = (right - left + 1) * histograms[i][j];
+            }
+          }
+        }
+      }
+
+      const userArea = Math.abs(answer[1][0] - answer[0][0] + 1) * Math.abs(answer[1][1] - answer[0][1] + 1);
+      return userArea >= maxArea;
+    },
+    convertAnswer: (ans) => {
+      try {
+        const parsed = JSON.parse(ans);
+        if (
+          Array.isArray(parsed) &&
+          parsed.length === 2 &&
+          typeof parsed[0][0] === "number" &&
+          typeof parsed[0][1] === "number" &&
+          typeof parsed[1][0] === "number" &&
+          typeof parsed[1][1] === "number"
+        ) {
+          return [parsed[0], parsed[1]];
+        }
+        return null;
+      } catch {
+        return null;
+      }
+    },
+    validateAnswer: (ans): ans is [[number, number], [number, number]] => {
+      return ans != null;
+    },
+  },
 };
-//   [CodingContractName.LargestRectangleIIMatrix]: {
-//     desc: (data: number[][]): string => {
-//       return [
-//         "MATRIX TAMEPLATE 279 279 279:\n\n",
-//         `  [${data.map((line) => `[${line}]`).join(",\n   ")}]\n\n`,
- 
-//         "Examples:\n\n",
-//         "    [[0,1,0,0,0],\n",
-//         "     [0,0,0,1,0]]\n",
-//         "\n",
-//         "Answer: '[[0,0],[0,1]]'\n\n",
-//         "    [[0,1],\n",
-//         "     [1,0]]\n",
-//         "\n",
-//         "Answer: '[[0,0],[0,0]]'",
-//       ].join(" ");
-//     },
-//     difficulty: 7,
-//     generate: (): (1 | 0)[][] => {
-//       const numRows: number = getRandomIntInclusive(4, 15);
-//       const numColumns: number = getRandomIntInclusive(4, 15);
-
-//       const grid: (1 | 0)[][] = [];
-//       grid.length = numRows;
-//       for (let i = 0; i < numRows; ++i) {
-//         grid[i] = [];
-//         grid[i].length = numColumns;
-//         grid[i].fill(0);
-//       }
-
-//       for (let r = 0; r < numRows; ++r) {
-//         for (let c = 0; c < numColumns; ++c) {
-//           if (r === 0 && c === 0) {
-//             continue;
-//           }
-//           if (r === numRows - 1 && c === numColumns - 1) {
-//             continue;
-//           }
-
-//           // 15% chance of an element being an obstacle
-//           if (Math.random() < 0.15) {
-//             grid[r][c] = 1;
-//           }
-//         }
-//       }
-
-//       return grid;
-//     },
-//     getAnswer: () => {
-//       return null;
-//     },
-//     solver: (data, answer) => {
-//       const width = data[0].length;
-//       const height = data.length;
-//       const dstY = height - 1;
-//       const dstX = width - 1;
-
-//       const distance: number[][] = new Array<number[]>(height);
-//       //const prev: [[number, number] | undefined][] = new Array(height);
-//       const queue: [number, number][] = [];
-
-//       for (let y = 0; y < height; y++) {
-//         distance[y] = new Array<number>(width).fill(Infinity);
-//         //prev[y] = new Array(width).fill(undefined) as [undefined];
-//       }
-
-//       function validPosition(y: number, x: number): boolean {
-//         return y >= 0 && y < height && x >= 0 && x < width && data[y][x] == 0;
-//       }
-
-//       // List in-bounds and passable neighbors
-//       function* neighbors(y: number, x: number): Generator<[number, number]> {
-//         if (validPosition(y - 1, x)) yield [y - 1, x]; // Up
-//         if (validPosition(y + 1, x)) yield [y + 1, x]; // Down
-//         if (validPosition(y, x - 1)) yield [y, x - 1]; // Left
-//         if (validPosition(y, x + 1)) yield [y, x + 1]; // Right
-//       }
-
-//       // Prepare starting point
-//       distance[0][0] = 0;
-//       queue.push([0, 0]);
-
-//       // Take next-nearest position and expand potential paths from there
-//       while (queue.length > 0) {
-//         const [y, x] = queue.shift() as [number, number];
-//         for (const [yN, xN] of neighbors(y, x)) {
-//           if (distance[yN][xN] == Infinity) {
-//             queue.push([yN, xN]);
-//             distance[yN][xN] = distance[y][x] + 1;
-//           }
-//         }
-//       }
-
-//       if (!Number.isFinite(distance[dstY][dstX])) return answer === "";
-//       if (answer.length > distance[dstY][dstX]) return false;
-
-//       let ansX = 0;
-//       let ansY = 0;
-//       for (const direction of answer.split("")) {
-//         switch (direction) {
-//           case "U":
-//             ansY -= 1;
-//             break;
-//           case "D":
-//             ansY += 1;
-//             break;
-//           case "L":
-//             ansX -= 1;
-//             break;
-//           case "R":
-//             ansX += 1;
-//             break;
-//           default:
-//             return false;
-//         }
-//       }
-
-//       return ansX === dstX && ansY === dstY;
-//     },
-//     convertAnswer: (ans) => ans.replace(/\s/g, ""),
-//     validateAnswer: (ans): ans is string =>
-//       typeof ans === "string" && ans.split("").every((c) => ["U", "D", "L", "R"].includes(c)),
-//   }
-
