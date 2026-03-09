fc62b91786501613e908d6b3053e893b0ee9262a
CatLover <152669316+catloversg@users.noreply.github.com>
2026-03-09 23:28:39 +0700

    Mitigate cyclic dependency issue in Jest test

--- src/PersonObjects/formulas/intelligence.ts
+++ src/PersonObjects/formulas/intelligence.ts
@@ -1,13 +1,13 @@
 import type { Person as IPerson } from "@nsdefs";
-import { PlayerObject } from "../Player/PlayerObject";
+import { Player } from "@player";
 
 export function calculateIntelligenceBonus(person: IPerson, weight = 1): number {
   return 1 + (weight * Math.pow(getEffectiveIntelligence(person), 0.8)) / 600;
 }
 
 export function getEffectiveIntelligence(person: IPerson): number {
-  if (!(person instanceof PlayerObject) || person.bitNodeOptions.intelligenceOverride === undefined) {
+  if (person !== Player || Player.bitNodeOptions.intelligenceOverride === undefined) {
     return person.skills.intelligence;
   }
-  return Math.min(person.bitNodeOptions.intelligenceOverride, person.skills.intelligence);
+  return Math.min(Player.bitNodeOptions.intelligenceOverride, person.skills.intelligence);
 }
