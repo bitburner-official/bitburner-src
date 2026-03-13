6ef152d4611629bb324994a19de5edbfe027fae0
CatLover <152669316+catloversg@users.noreply.github.com>
2026-03-13 12:49:59 +0700

    Apply intelligenceOverride to both Player and sleeves

--- src/PersonObjects/formulas/intelligence.ts
+++ src/PersonObjects/formulas/intelligence.ts
@@ -1,12 +1,13 @@
 import type { Person as IPerson } from "@nsdefs";
 import { Player } from "@player";
+import { Person } from "../Person";
 
 export function calculateIntelligenceBonus(person: IPerson, weight = 1): number {
   return 1 + (weight * Math.pow(getEffectiveIntelligence(person), 0.8)) / 600;
 }
 
 export function getEffectiveIntelligence(person: IPerson): number {
-  if (person !== Player || Player.bitNodeOptions.intelligenceOverride === undefined) {
+  if (!(person instanceof Person) || Player.bitNodeOptions.intelligenceOverride === undefined) {
     return person.skills.intelligence;
   }
   return Math.min(Player.bitNodeOptions.intelligenceOverride, person.skills.intelligence);
