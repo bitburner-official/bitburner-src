4cd40462eeabfa79778d67400207d25f5b3c91a2
CatLover <152669316+catloversg@users.noreply.github.com>
2026-03-14 20:50:01 +0700

    UI: Prevent joining banned factions via UI

--- src/Faction/ui/FactionsRoot.tsx
+++ src/Faction/ui/FactionsRoot.tsx
@@ -75,9 +75,11 @@ const FactionElement = (props: FactionElementProps): React.ReactElement => {
     Router.toPage(Page.FactionAugmentations, { faction });
   }
 
-  function acceptInvitation(event: React.MouseEvent<HTMLButtonElement>, faction: FactionName): void {
-    if (!event.isTrusted) return;
-    joinFaction(Factions[faction]);
+  function acceptInvitation(event: React.MouseEvent<HTMLButtonElement>, faction: Faction): void {
+    if (!event.isTrusted || !faction.alreadyInvited || faction.isBanned) {
+      return;
+    }
+    joinFaction(faction);
     props.rerender();
   }
 
@@ -106,7 +108,7 @@ const FactionElement = (props: FactionElementProps): React.ReactElement => {
             <Button onClick={() => openFactionAugPage(props.faction)}>Augments</Button>
           </Box>
         ) : props.faction.alreadyInvited ? (
-          <Button sx={{ height: "48px", mr: 1 }} onClick={(e) => acceptInvitation(e, props.faction.name)}>
+          <Button sx={{ height: "48px", mr: 1 }} onClick={(e) => acceptInvitation(e, props.faction)}>
             Join!
           </Button>
         ) : null}
