8e5a9850b05e9cabe23587926b21f7f578610f05
CatLover <152669316+catloversg@users.noreply.github.com>
2026-03-20 20:12:37 +0700

    CODEBASE: Configure Babel to use core-js polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ["@babel/preset-react", "@babel/preset-env", "@babel/preset-typescript"],
+  // The default value of sourceType is "module", so we can omit this config. We can also specify "unambiguous" to let
+  // Babel infer the source type and avoid explicitly setting it for saveDataBinaryFormat.js. However, being explicit is
+  // preferable, since we know all of our modules use ES modules except for those in the Electron folder.
+  sourceType: "module",
+  presets: [
+    "@babel/preset-react",
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: "3.49",
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+  overrides: [
+    {
+      test: ["./electron/saveDataBinaryFormat.js"],
+      sourceType: "commonjs",
+    },
+  ],
 };
