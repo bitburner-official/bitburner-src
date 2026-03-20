9de9fbaf27870e4c30a468dc7626d0a145defc66
catloversg <152669316+catloversg@users.noreply.github.com>
2026-03-20 10:14:16 +0700

    CODEBASE: Fix React warning in IPvGO scoring explanation popup (#2581)

--- src/Go/ui/GoScoreExplanation.tsx
+++ src/Go/ui/GoScoreExplanation.tsx
@@ -16,7 +16,7 @@ export const GoScoreExplanation = ({ open, onClose }: Props): React.ReactElement
     <Modal open={open} onClose={onClose}>
       <>
         <div className={classes.scoreExplanationModal}>
-          <Typography>
+          <Typography component="div">
             <h2>IPvGO Scoring Explanation</h2>
             IPvGO uses one of the oldest scoring systems in Go, "area scoring", rather than "territory scoring" later
             popularized by Japan. All stones are alive unless captured, chains that could be dead are not automatically
