55a54c0573b5faaa37639684929792c153273781
CatLover <152669316+catloversg@users.noreply.github.com>
2026-03-21 00:24:54 +0700

    Feedback

--- electron/saveDataBinaryFormat.d.ts
+++ electron/saveDataBinaryFormat.d.ts
@@ -1,4 +1,3 @@
-export declare const encodeBytesToBase64String: (bytes: Uint8Array<ArrayBuffer>) => string;
 export declare const decodeBase64BytesToBytes: (bytes: Uint8Array<ArrayBuffer>) => Uint8Array<ArrayBuffer>;
 export declare const isBinaryFormat: (saveData: string | Uint8Array<ArrayBuffer>) => boolean;
 export declare const isSteamCloudFormat: (saveData: string | Uint8Array<ArrayBuffer>) => boolean;
