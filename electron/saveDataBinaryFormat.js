703605c7912b9c957ea2dcf49c1df55b29dd8d7a
CatLover <152669316+catloversg@users.noreply.github.com>
2026-03-21 21:20:08 +0700

    Revert "Feedback"
    This reverts commit 55a54c0573b5faaa37639684929792c153273781.

--- electron/saveDataBinaryFormat.js
+++ electron/saveDataBinaryFormat.js
@@ -1,16 +1,37 @@
 // The 2 magic bytes of the gzip header plus the mandatory compression type of DEFLATE
 const magicBytesOfDeflateGzip = new Uint8Array([0x1f, 0x8b, 0x08]);
 // Base64-encoded string of magicBytesOfDeflateGzip
-const base64EncodingOfMagicBytes = magicBytesOfDeflateGzip.toBase64();
+const base64EncodingOfMagicBytes = encodeBytesToBase64String(magicBytesOfDeflateGzip);
 // Convert the base64-encoded string to a byte array
 const byteArrayOfBase64EncodingOfMagicBytes = Uint8Array.from(base64EncodingOfMagicBytes, (c) => c.charCodeAt(0));
+
+/**
+ * @param {Uint8Array} bytes
+ * @returns {string}
+ */
+function encodeBytesToBase64String(bytes) {
+  let binaryString = "";
+  for (let i = 0; i < bytes.length; i++) {
+    binaryString += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binaryString);
+}
 
 /**
  * @param {Uint8Array} bytes
  * @returns {Uint8Array}
  */
 function decodeBase64BytesToBytes(bytes) {
-  return Uint8Array.fromBase64(new TextDecoder("windows-1252").decode(bytes));
+  let base64String = "";
+  for (let i = 0; i < bytes.length; i++) {
+    base64String += String.fromCharCode(bytes[i]);
+  }
+  const decodedBinaryString = atob(base64String);
+  const result = new Uint8Array(decodedBinaryString.length);
+  for (let i = 0; i < decodedBinaryString.length; i++) {
+    result[i] = decodedBinaryString.charCodeAt(i);
+  }
+  return result;
 }
 
 /**
@@ -44,4 +65,4 @@ function isSteamCloudFormat(rawData) {
   return true;
 }
 
-module.exports = { decodeBase64BytesToBytes, isBinaryFormat, isSteamCloudFormat };
+module.exports = { encodeBytesToBase64String, decodeBase64BytesToBytes, isBinaryFormat, isSteamCloudFormat };
