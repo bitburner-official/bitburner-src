3224684b2f8711b1f582ae47ef4ac8d5a366a4fd
Michael Ficocelli <ficocemt@gmail.com>
2026-04-03 21:17:04 -0400

    DNET: Remove bonus time effect on authentication and heartbleed speed

--- src/DarkNet/effects/effects.ts
+++ src/DarkNet/effects/effects.ts
@@ -79,7 +79,6 @@ export const calculateAuthenticationTime = (
   const underleveledFactor = applyUnderleveledFactor ? 1.5 + (chaRequired + 50) / (person.skills.charisma + 50) : 1;
   const hasBootsFactor = Player.hasAugmentation(AugmentationName.TheBoots) ? 0.8 : 1;
   const hasSf15_2Factor = Player.activeSourceFileLvl(15) > 2 ? 0.8 : 1;
-  const bonusTimeFactor = hasDarknetBonusTime() ? 0.75 : 1;
 
   const time =
     baseTime *
@@ -88,7 +87,6 @@ export const calculateAuthenticationTime = (
     underleveledFactor *
     hasBootsFactor *
     hasSf15_2Factor *
-    bonusTimeFactor *
     threadsFactor;
 
   // We need to call GetServer and check if it's a dnet server later because this function can be called by formulas
