c1431d9f06fc325b5af409bdb4dcc5cd10024389
CatLover <152669316+catloversg@users.noreply.github.com>
2026-04-11 11:48:23 +0700

    Revert "UI: Add button to manually check achievements"
    This reverts commit 70ab70ec68115440836f5f4f4ce7f864ef2470be.

--- src/Achievements/AchievementsRoot.tsx
+++ src/Achievements/AchievementsRoot.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 
 import { AchievementList } from "./AchievementList";
-import { achievements, calculateAchievements } from "./Achievements";
-import { Box, Button, Typography } from "@mui/material";
+import { achievements } from "./Achievements";
+import { Box, Typography } from "@mui/material";
 import { Player } from "@player";
+import { makeStyles } from "tss-react/mui";
+
+const useStyles = makeStyles()({
+  root: {
+    width: 50,
+    userSelect: "none",
+  },
+});
 
 export function AchievementsRoot(): JSX.Element {
+  const { classes } = useStyles();
   return (
-    <div style={{ width: "100%" }}>
+    <div className={classes.root} style={{ width: "100%" }}>
       <Typography variant="h4">Achievements</Typography>
       <Box mx={2}>
         <Typography>
           Achievements are persistent rewards for various actions and challenges. A limited number of Bitburner
           achievements have corresponding achievements in Steam.
-          <br />
-          Achievements are automatically checked periodically. You can also manually check with the button below.
         </Typography>
-        <Button onClick={() => calculateAchievements()}>Check achievements</Button>
         <AchievementList achievements={Object.values(achievements)} playerAchievements={Player.achievements} />
       </Box>
     </div>
