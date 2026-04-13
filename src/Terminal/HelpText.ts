cb14655325752f7303dbe08a1cc19bcba4f13ad2
catloversg <152669316+catloversg@users.noreply.github.com>
2026-04-13 07:06:27 +0700

    MISC: Update description of "cat" command (#2654)

--- src/Terminal/HelpText.ts
+++ src/Terminal/HelpText.ts
@@ -5,7 +5,7 @@ export const TerminalHelpText: string[] = [
   "    analyze                          Get information about the current machine ",
   "    backdoor                         Install a backdoor on the current machine ",
   "    buy [-l/-a/program]              Purchase a program through the Dark Web",
-  "    cat [file]                       Display a .msg, .lit, or text file",
+  "    cat [file]                       Display the contents of a file",
   "    cd [dir]                         Change to a new directory",
   "    changelog                        Display changelog",
   "    check [script] [args...]         Print a script's logs to Terminal",
