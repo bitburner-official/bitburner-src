1ffbe8a1c90da9905a4d3b98d3afa4501a9dc15f
Mathekatze <118079958+Mathekatze@users.noreply.github.com>
2026-04-16 21:44:28 +0200

    refactor PID generation logic

--- src/Netscript/Pid.ts
+++ src/Netscript/Pid.ts
@@ -9,29 +9,28 @@ export function generateNextPid(): number {
 
   // Cap the number of search iterations at some arbitrary value to avoid
   // infinite loops. We'll assume that players won't have a million running scripts.
-  for (let attemptCounter = 0; attemptCounter < 1e6; ++attemptCounter) {
+  for (let attemptCounter = 0; attemptCounter < 1e6; ++attemptCounter, ++pidCandidate) {
+    // ensure the candidate PID is a safe integer
+    if (pidCandidate >= Number.MAX_SAFE_INTEGER) {
+      pidCandidate = 1;
+    }
+    // ensure the PID is not in use
+    if (workerScripts.has(pidCandidate)) {
+      continue;
+    }
     /* Also check the recentScripts: The function `AddRecentScript` de-duplicates scripts by PID.
        * Therefore scripts which re-use a PID from that list do not show up the list of recent scripts
        * upon termination (if the previous script with that PID is still in the list at that point).
        * Such a PID re-use could happen after installing augmentations, which resets the PID counter
        * but does not clear the list recent scripts. */
-    if (!workerScripts.has(pidCandidate)
-         && recentScripts.some((r) => (r.runningScript.pid === pidCandidate))) {
-      // found a PID that's not in use
-      // set the counter to the successor of the found PID, wrapping back to 1 when it gets too high
-      pidCounter = pidCandidate + 1;
-      if (pidCounter >= Number.MAX_SAFE_INTEGER) {
-        pidCounter = 1;
-      }
-      return pidCandidate;
+    if(recentScripts.some((r) => (r.runningScript.pid === pidCandidate))) {
+      continue;
     }
-
-    ++pidCandidate;
-    if (pidCandidate >= Number.MAX_SAFE_INTEGER) {
-      pidCandidate = 1;
-    }
+    // found a PID that's not in use
+    pidCounter = pidCandidate + 1;
+    return pidCandidate;
   }
-  // ran out of attempts without finding a valid unused pid :-(
+  // ran out of attempts without finding an unused PID :-(
   return -1;
 }
 
