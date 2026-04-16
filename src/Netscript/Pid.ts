d121cc6a8b90d982fde9a89bbb4e3e0396808575
Mathekatze <118079958+Mathekatze@users.noreply.github.com>
2026-04-16 21:12:54 +0200

    improve PID generation
    The previous implementation only checked that `i` does not exceed `MAX_SAFE_INTEGER` but then returned `tempCounter + i` without checking that. Also if `i` were to exceed `MAX_SAFE_INTEGER` (which was impossible due to the loop condition) it would have reset the loop variable, potentially causing an infinite loop if someone were to increase the bound for the loop past `MAX_SAFE_INTEGER`.

--- src/Netscript/Pid.ts
+++ src/Netscript/Pid.ts
@@ -4,35 +4,28 @@ let pidCounter = 1;
 
 /** Find and return the next available PID for a script */
 export function generateNextPid(): number {
-  let tempCounter = pidCounter;
+  let pidCandidate = pidCounter;
 
   // Cap the number of search iterations at some arbitrary value to avoid
-  // infinite loops. We'll assume that players wont have 1mil+ running scripts
-  let found = false;
-  for (let i = 0; i < 1e6; ) {
-    if (!workerScripts.has(tempCounter + i)) {
-      found = true;
-      tempCounter = tempCounter + i;
-      break;
+  // infinite loops. We'll assume that players won't have a million running scripts.
+  for (let attemptCounter = 0; attemptCounter < 1e6; ++attemptCounter) {
+    if (!workerScripts.has(pidCandidate)) {
+      // found a PID that's not in use
+      // set the counter to the successor of the found PID, wrapping back to 1 when it gets too high
+      pidCounter = pidCandidate + 1;
+      if (pidCounter >= Number.MAX_SAFE_INTEGER) {
+        pidCounter = 1;
+      }
+      return pidCandidate;
     }
 
-    if (i === Number.MAX_SAFE_INTEGER - 1) {
-      i = 1;
-    } else {
-      ++i;
+    ++pidCandidate;
+    if (pidCandidate >= Number.MAX_SAFE_INTEGER) {
+      pidCandidate = 1;
     }
   }
-
-  if (found) {
-    pidCounter = tempCounter + 1;
-    if (pidCounter >= Number.MAX_SAFE_INTEGER) {
-      pidCounter = 1;
-    }
-
-    return tempCounter;
-  } else {
-    return -1;
-  }
+  // ran out of attempts without finding a valid unused pid :-(
+  return -1;
 }
 
 export function resetPidCounter(): void {
