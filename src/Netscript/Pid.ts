ee19885d5dda9d9679715b87e73d4290f0b1d801
Mathekatze <118079958+Mathekatze@users.noreply.github.com>
2026-04-16 21:35:33 +0200

    fix scripts not showing up in list of recent scripts after installing augmentations
    Installing augmentations resets the PID counter, but does not clear the list of recently finished scripts. Re-use of PIDs which are in that list results in script instances not showing up in the list of recently finished scripts due to some de-duplication logic there.
    This checking PIDs against that list prevents such duplicates.

--- src/Netscript/Pid.ts
+++ src/Netscript/Pid.ts
@@ -1,4 +1,5 @@
 import { workerScripts } from "./WorkerScripts";
+import { recentScripts } from "./Netscript/RecentScripts";
 
 let pidCounter = 1;
 
@@ -9,7 +10,13 @@ export function generateNextPid(): number {
   // Cap the number of search iterations at some arbitrary value to avoid
   // infinite loops. We'll assume that players won't have a million running scripts.
   for (let attemptCounter = 0; attemptCounter < 1e6; ++attemptCounter) {
-    if (!workerScripts.has(pidCandidate)) {
+    /* Also check the recentScripts: The function `AddRecentScript` de-duplicates scripts by PID.
+       * Therefore scripts which re-use a PID from that list do not show up the list of recent scripts
+       * upon termination (if the previous script with that PID is still in the list at that point).
+       * Such a PID re-use could happen after installing augmentations, which resets the PID counter
+       * but does not clear the list recent scripts. */
+    if (!workerScripts.has(pidCandidate)
+         && recentScripts.some((r) => (r.runningScript.pid === pidCandidate))) {
       // found a PID that's not in use
       // set the counter to the successor of the found PID, wrapping back to 1 when it gets too high
       pidCounter = pidCandidate + 1;
