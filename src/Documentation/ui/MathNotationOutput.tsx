362986fdfaf15d18885ba6cb5f33b7544a8d52b6
Mathekatze <118079958+Mathekatze@users.noreply.github.com>
2026-05-16 22:25:53 +0200

    allow passing style through MathNotationOutput to Typography

--- src/Documentation/ui/MathNotationOutput.tsx
+++ src/Documentation/ui/MathNotationOutput.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import { convertMathNotation } from "../root";
 
-export function MathNotationOutput({ notation }: { notation: string }): JSX.Element {
+export function MathNotationOutput({ notation, sx }: { notation: string; sx?: any }): JSX.Element {
   // It's fine to use dangerouslySetInnerHTML here. We control the data in both MathNotation.json and
   // MathNotationOutput.json. They are not user-provided data.
-  return <Typography dangerouslySetInnerHTML={{ __html: convertMathNotation(notation) }} />;
+  return <Typography dangerouslySetInnerHTML={{ __html: convertMathNotation(notation) }} sx={sx} />;
 }
