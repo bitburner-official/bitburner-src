427e5dda5e4fed9749f4ee4257fc70c380975cce
Mathekatze <118079958+Mathekatze@users.noreply.github.com>
2026-05-17 01:26:04 +0200

    fix linter error

--- src/Documentation/ui/MathNotationOutput.tsx
+++ src/Documentation/ui/MathNotationOutput.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import { convertMathNotation } from "../root";
+import type { SxProps, Theme } from "@mui/system";
 
-export function MathNotationOutput({ notation, sx }: { notation: string; sx?: any }): JSX.Element {
+export function MathNotationOutput({ notation, sx }: { notation: string; sx?: SxProps<Theme> }): JSX.Element {
   // It's fine to use dangerouslySetInnerHTML here. We control the data in both MathNotation.json and
   // MathNotationOutput.json. They are not user-provided data.
   return <Typography dangerouslySetInnerHTML={{ __html: convertMathNotation(notation) }} sx={sx} />;
