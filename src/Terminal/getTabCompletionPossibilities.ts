1fa9ccd9e7c360a922c8cac7ebbaaa850f9f71dc
Olacken <olacocken@gmail.com>
2026-05-19 07:22:09 +0200

    Update getTabCompletionPossibilities.ts
    Rename path to absoluteDir to be clearer on the purpose

--- src/Terminal/getTabCompletionPossibilities.ts
+++ src/Terminal/getTabCompletionPossibilities.ts
@@ -52,15 +52,15 @@ export async function getTabCompletionPossibilities(terminalText: string, baseDi
 
   /** The directory portion of the current input */
   let relativeDir = "";
-  let path = baseDir;
+  let absoluteDir = baseDir;
   const slashIndex = currentText.lastIndexOf("/");
 
   if (slashIndex !== -1) {
     relativeDir = currentText.substring(0, slashIndex + 1);
-    path = resolveDirectory(relativeDir, baseDir);
+    absoluteDir = resolveDirectory(relativeDir, baseDir);
     // No valid terminal inputs contain a / that does not indicate a path
-    if (path === null) return [];
-    pathingRequiredMatch = currentText.replace(/^.*\//, path).toLowerCase();
+    if (absoluteDir === null) return [];
+    pathingRequiredMatch = currentText.replace(/^.*\//, absoluteDir).toLowerCase();
   } else if (baseDir !== root) {
     pathingRequiredMatch = (baseDir + currentText).toLowerCase();
   }
@@ -84,7 +84,7 @@ export async function getTabCompletionPossibilities(terminalText: string, baseDi
     for (const member of iterable) {
       if (ignoreCurrent && member.length <= requiredStart.length) continue;
       if (member.toLowerCase().startsWith(requiredStart)) {
-        possibilities.push(usePathing ? relativeDir + member.substring(path.length) : member);
+        possibilities.push(usePathing ? relativeDir + member.substring(absoluteDir.length) : member);
       }
     }
   }
