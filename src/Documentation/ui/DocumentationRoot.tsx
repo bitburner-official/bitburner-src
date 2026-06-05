90de3200962b57bfba35ecf202132d8ec10bf0a7
Shane Creighton-Young <shaneecy@gmail.com>
2026-06-05 05:21:16 -0400

    UI: Disable docs back button if empty history (#2850)

--- src/Documentation/ui/DocumentationRoot.tsx
+++ src/Documentation/ui/DocumentationRoot.tsx
@@ -82,7 +82,9 @@ export function DocumentationRoot({ docPage }: { docPage?: string }): React.Reac
         bgcolor={Settings.theme.backgroundprimary}
         alignItems="center"
       >
-        <Button onClick={() => history.pop()}>Back</Button>
+        <Button onClick={() => history.pop()} disabled={history.pages.length === 0}>
+          Back
+        </Button>
         <Button onClick={() => history.home()}>Home</Button>
         <DocumentationAutocomplete
           sx={{ marginLeft: "10px" }}
