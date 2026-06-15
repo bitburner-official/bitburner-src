ec5f2a248abf80cfc2dcbbb27cd2a559ff139875
David Walker <d0sboots@gmail.com>
2026-06-15 11:35:55 -0700

    REFACTOR: Remove permanent reference to the saveObject (#2885)
    * REFACTOR: Remove permanent reference to the saveObject
    
    This moves almost all the functions out of BitburnerSaveObject and turns
    them into free functions instead. Most of them didn't need to be
    instance members at all, and the one that definitely used the fields is
    better off making a new instance every time. BitburnerSaveObject is new
    a dumb data object that exists only for serialization purposes, and all
    the functionality is accessed via importing the specific functions that
    are desired.
    
    I also removed the permanent reference to save/load stuff in dev on the
    window object, since removing permanent references is sort of the point.
    It's not really needed given what we have in SF4 now, anyway.
    
    This saves massively on persistent memory usage when the save is large.
    There is still a big memory spike when saving/loading, but it is at
    least reclaimable now.
    
    * Fix lint
    
    ---------
    
    Co-authored-by: CatLover <152669316+catloversg@users.noreply.github.com>

--- src/ui/GameRoot.tsx
+++ src/ui/GameRoot.tsx
@@ -5,7 +5,7 @@ import { makeStyles } from "tss-react/mui";
 
 import { Player } from "@player";
 import { installAugmentations } from "../Augmentation/AugmentationHelpers";
-import { saveObject } from "../SaveObject";
+import { saveGame, exportGame } from "../SaveObject";
 import { CompletedProgramName, LocationName, SimplePage } from "@enums";
 import { ITutorial, iTutorialStart } from "../InteractiveTutorial";
 import { InteractiveTutorialRoot } from "./InteractiveTutorial/InteractiveTutorialRoot";
@@ -221,8 +221,7 @@ export function GameRoot(): React.ReactElement {
     for (const server of GetAllServers(true)) {
       server.runningScriptMap.clear();
     }
-    saveObject
-      .saveGame()
+    saveGame()
       .then(() => {
         setTimeout(() => htmlLocation.reload(), 0);
       })
@@ -442,10 +441,10 @@ export function GameRoot(): React.ReactElement {
         <GameOptionsRoot
           tab={pageWithContext.tab}
           save={() => {
-            saveObject.saveGame().catch((error) => exceptionAlert(error));
+            saveGame().catch((error) => exceptionAlert(error));
           }}
           export={() => {
-            saveObject.exportGame().catch((error) => exceptionAlert(error));
+            exportGame().catch((error) => exceptionAlert(error));
           }}
           forceKill={killAllScripts}
           softReset={softReset}
@@ -464,7 +463,7 @@ export function GameRoot(): React.ReactElement {
       mainPage = (
         <AugmentationsRoot
           exportGameFn={() => {
-            saveObject.exportGame().catch((error) => exceptionAlert(error));
+            exportGame().catch((error) => exceptionAlert(error));
           }}
           installAugmentationsFn={() => {
             installAugmentations();
@@ -540,7 +539,7 @@ export function GameRoot(): React.ReactElement {
                     <CharacterOverview
                       parentOpen={parentOpen}
                       save={() => {
-                        saveObject.saveGame().catch((error) => exceptionAlert(error));
+                        saveGame().catch((error) => exceptionAlert(error));
                       }}
                       killScripts={killAllScripts}
                     />
