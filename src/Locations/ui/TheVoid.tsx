a52434890c338f0ec6ab37047e9b7db3d39904bc
Enric <enricgalerarotger@gmail.com>
2026-07-07 13:34:13 +0200

    Remove unused import

--- src/Locations/ui/TheVoid.tsx
+++ src/Locations/ui/TheVoid.tsx
@@ -1,5 +1,4 @@
 import { LocationName } from "@enums";
-import { gotoLocation } from "src/PersonObjects/Player/PlayerObjectGeneralMethods";
 import { Player } from "@player";
 import React, { useEffect } from "react";
 export function TheVoid(): React.ReactElement {
