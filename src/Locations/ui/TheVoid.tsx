35332a4a600ef1c0af30b759c68fe14a398e8d9d
Enric <enricgalerarotger@gmail.com>
2026-07-07 12:01:10 +0200

    Prettify

--- src/Locations/ui/TheVoid.tsx
+++ src/Locations/ui/TheVoid.tsx
@@ -3,8 +3,8 @@ import { gotoLocation } from "src/PersonObjects/Player/PlayerObjectGeneralMethod
 import { Player } from "@player";
 import React, { useEffect } from "react";
 export function TheVoid(): React.ReactElement {
-    useEffect(() => {
-        Player.gotoLocation(LocationName.Void);
-    })
-    return <></>;
+  useEffect(() => {
+    Player.gotoLocation(LocationName.Void);
+  });
+  return <></>;
 }
