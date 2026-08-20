55810bade167ca32fbeb2761af81eb6cfbc26aba
catloversg <152669316+catloversg@users.noreply.github.com>
2026-08-21 04:17:24 +0700

    UI: Rework RFA connection indicator and RFA setting page (#3050)

--- src/GameOptions/ui/RemoteFileApiConnectionStatus.tsx
+++ src/GameOptions/ui/RemoteFileApiConnectionStatus.tsx
@@ -1,41 +1,88 @@
 import React, { useState, useEffect } from "react";
 import { Box, IconButton, Tooltip, Typography } from "@mui/material";
-import { getRemoteFileApiConnectionStatus } from "../../RemoteFileAPI/RemoteFileAPI";
+import {
+  canCreateNewRemoteFileApiConnection,
+  closeRemoteFileApiConnection,
+  getRemoteFileApiConnectionStatus,
+  newRemoteFileApiConnection,
+} from "../../RemoteFileAPI/RemoteFileAPI";
 import OnlinePredictionIcon from "@mui/icons-material/OnlinePrediction";
 import { Settings } from "../../Settings/Settings";
 import { Router } from "../../ui/GameRoot";
 import { Page } from "../../ui/Router";
-import { RemoteFileApiConnectionEvents } from "../../RemoteFileAPI/Remote";
+import { RemoteFileApiConnectionEvents, RemoteFileApiConnectionSettingEvents } from "../../RemoteFileAPI/Remote";
+import { useRerender } from "../../ui/React/hooks";
 
 export const RemoteFileApiConnectionStatus = ({ showIcon }: { showIcon: boolean }): React.ReactElement => {
   const [rfaConnectionStatus, setRfaConnectionStatus] = useState(getRemoteFileApiConnectionStatus());
+  const rerender = useRerender();
 
-  useEffect(
-    () =>
-      RemoteFileApiConnectionEvents.subscribe((status) => {
-        setRfaConnectionStatus(status);
-      }),
-    [],
-  );
+  useEffect(() => {
+    const unsubscriberForRFAEvents = RemoteFileApiConnectionEvents.subscribe((status) => {
+      setRfaConnectionStatus(status);
+    });
+    const unsubscriberForRFASettingEvents = RemoteFileApiConnectionSettingEvents.subscribe(() => {
+      rerender();
+    });
+    return () => {
+      unsubscriberForRFAEvents();
+      unsubscriberForRFASettingEvents();
+    };
+  }, [rerender]);
 
-  let color;
-  switch (rfaConnectionStatus) {
-    case "Online":
-      color = Settings.theme.success;
-      break;
-    case "Offline":
-      color = Settings.theme.error;
-      break;
-    case "Reconnecting":
-      color = Settings.theme.warning;
-      break;
-  }
+  const connectionConfig = {
+    Online: { color: Settings.theme.success, instruction: "Click to disconnect" },
+    Offline: {
+      color: Settings.theme.error,
+      instruction: canCreateNewRemoteFileApiConnection() ? "Click to connect" : "Click to go to the option page",
+    },
+    Reconnecting: {
+      color: Settings.theme.warning,
+      instruction: canCreateNewRemoteFileApiConnection()
+        ? "Click to try to connect immediately without waiting"
+        : "Click to go to the option page",
+    },
+    Connecting: {
+      color: Settings.theme.warning,
+      instruction: "Connecting",
+    },
+  };
+
+  const color = connectionConfig[rfaConnectionStatus].color;
 
   return (
     <Box style={{ display: "flex", flex: 1, justifyContent: "flex-start", alignItems: "center" }}>
       {showIcon ? (
-        <IconButton aria-label="Remote API status" onClick={() => Router.toPage(Page.Options, { tab: "Remote API" })}>
-          <Tooltip title={`Remote API: ${rfaConnectionStatus}`}>
+        <IconButton
+          aria-label="Remote API status"
+          onClick={() => {
+            switch (rfaConnectionStatus) {
+              case "Online":
+                closeRemoteFileApiConnection();
+                break;
+              case "Offline":
+              case "Reconnecting":
+                if (canCreateNewRemoteFileApiConnection()) {
+                  newRemoteFileApiConnection();
+                } else {
+                  Router.toPage(Page.Options, { tab: "Remote API" });
+                }
+                break;
+              case "Connecting":
+                // Do nothing.
+                break;
+            }
+          }}
+        >
+          <Tooltip
+            title={
+              <>
+                Remote API: {rfaConnectionStatus}
+                <br />
+                {connectionConfig[rfaConnectionStatus].instruction}
+              </>
+            }
+          >
             <OnlinePredictionIcon style={{ fontSize: "30px", color }} />
           </Tooltip>
         </IconButton>
