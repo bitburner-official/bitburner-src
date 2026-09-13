0ed708e31c6cd39be625055cc94363c3a3ea249e
branchtheory <robert.g.silverstone@gmail.com~>
2026-09-14 00:08:20 +0100

    .

--- src/Hacknet/ui/GeneralInfo.tsx
+++ src/Hacknet/ui/GeneralInfo.tsx
@@ -21,8 +21,8 @@ export function GeneralInfo(props: IProps): React.ReactElement {
       {!props.hasHacknetServers ? (
         <>
           <Typography>
-            Here you can purchase Hacknet Nodes, specialized machines that can anonymously connect and contribute their
-            resoures to the network. They allow you to take a small percentage of profits from hacks performed on the
+            Here you can purchase Hacknet Nodes, specialized machines that can connect and contribute their resoures to
+            the Hacknet network. They allow you to take a small percentage of profits from hacks performed on the
             network. Essentially, you are renting out your Nodes' computing power.
             <br />
             <br />
