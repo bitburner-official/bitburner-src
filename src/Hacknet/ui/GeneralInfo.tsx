b582b5763b713bfc4002911d5e443d8b4d98ec69
branchtheory <97463314+branchtheory@users.noreply.github.com>
2026-09-23 22:32:05 +0100

    MISC: Hacknet text (#3099)

--- src/Hacknet/ui/GeneralInfo.tsx
+++ src/Hacknet/ui/GeneralInfo.tsx
@@ -14,36 +14,32 @@ export function GeneralInfo(props: IProps): React.ReactElement {
   return (
     <>
       <Typography>
-        The Hacknet is a global, decentralized network of machines. It is used by hackers all around the world to
-        anonymously share computing power and perform distributed cyberattacks without the fear of being traced.
+        The Hacknet is a global, decentralized network of machines. It is used by hackers around the world to perform
+        cyberattacks without the fear of being traced.
       </Typography>
       <br />
       {!props.hasHacknetServers ? (
         <>
           <Typography>
-            {`Here, you can purchase a Hacknet Node, a specialized machine that can connect ` +
-              `and contribute its resources to the Hacknet network. This allows you to take ` +
-              `a small percentage of profits from hacks performed on the network. Essentially, ` +
-              `you are renting out your Node's computing power.`}
-          </Typography>
-          <Typography>
-            {`Each Hacknet Node you purchase will passively earn you money. Each Hacknet Node ` +
-              `can be upgraded in order to increase its computing power and thereby increase ` +
-              `the profit you earn from it.`}
+            Here you can purchase Hacknet Nodes, specialized machines that can connect and contribute their resoures to
+            the Hacknet network. They allow you to take a small percentage of profits from hacks performed on the
+            network. Essentially, you are renting out your Nodes' computing power.
+            <br />
+            <br />
+            Hacknet Nodes passively earn you money, even when you're offline. You can upgrade Hacknet Nodes to increase
+            their earnings.
           </Typography>
         </>
       ) : (
         <>
           <Typography>
-            {`Here, you can purchase a Hacknet Server, an upgraded version of the Hacknet Node. ` +
-              `Hacknet Servers will perform computations and operations on the network, earning ` +
-              `you hashes. Hashes can be spent on a variety of different upgrades.`}
-          </Typography>
-          <Typography>
-            {`Hacknet Servers can also be used as servers to run scripts. However, running scripts ` +
-              `on a server will reduce its hash rate (hashes generated per second). A Hacknet Server's hash ` +
-              `rate will be reduced by the percentage of RAM that is being used by that Server to run ` +
-              `scripts.`}
+            Here you can purchase Hacknet Servers, an upgraded version of the Hacknet Node. Hacknet Servers perform
+            computations and operations on the network, earning you hashes. Hashes can be spent on a variety of
+            upgrades.
+            <br />
+            <br />
+            Hacknet Servers can be used to run scripts, just like regular servers. However, running scripts on a Hacknet
+            Server reduces its hash rate by the percentage of its RAM you use.
           </Typography>
         </>
       )}
